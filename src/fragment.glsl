cfb90e1afad4d84daaf58fd89e9793217eeaf6dd
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-03 06:13:48 +0900

    #02 時間で横ずれさせる。sinで振幅させてuTickをつかう

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -1,19 +1,17 @@
+varying vec2 vUv;
+uniform sampler2D uTex;
+uniform float uTick;
 
-    varying vec2 vUv;
-    uniform sampler2D uTex;
-    uniform float uTime;
-
-    void main() {
-      vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
+void main() {
+  float time = uTick * 0.01;
+  vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
       // vec4 texColor = texture(uTex, vUv);
       // gl_FragColor = texColor;
 
-      color.r = texture(uTex, vUv + vec2(-0.1, 0.0)).r;
-      color.g = texture(uTex, vUv + vec2(0.0, 0.0)).g;
-      color.b = texture(uTex, vUv + vec2(0.1, 0.0)).b;
-      
-      gl_FragColor = color;
+  color.r = texture(uTex, vUv + vec2(sin(time), 0.0)).r;
+  color.g = texture(uTex, vUv + vec2(sin(time), 0.0)).g;
+  color.b = texture(uTex, vUv + vec2(sin(time), 0.0)).b;
 
-    }
-    
-    
+  gl_FragColor = color;
+
+}
