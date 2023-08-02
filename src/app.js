3b3a3a8920064e8554333eac947ffaed718deed0
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-02 17:49:39 +0900

    chore:#1 ドットのギザギザをなくすため

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ async function init() {
     1000
   );
 
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
