87e365d0d41d9abbcf849289597afc0a3956ed42
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-04 11:43:53 +0900

    fix:#07 not add a semicolon

--- src/slider0/fragment.glsl
+++ src/slider0/fragment.glsl
@@ -8,13 +8,17 @@ uniform sampler2D uTexCurrent;
 uniform sampler2D uTexNext;
 uniform float uTick;
 uniform vec2 uNoiseScale;
+uniform float uProgress;
 
 void main() {
 
   // ヨコシマのノイズ
-   float n = noise3(vec3(vUv.x * uNoiseScale.x, vUv.y * uNoiseScale.y, uTick * 0.01));
+  float n = noise3(vec3(vUv.x * uNoiseScale.x, vUv.y * uNoiseScale.y, uTick * 0.01));
 
-  vec4 tex1 = texture(uTexCurrent, vUv);
-  vec4 tex2 = texture(uTexNext, vUv);
-  gl_FragColor = tex2;
-}
+  vec4 texCurrent = texture(uTexCurrent, vUv);
+  vec4 texNext = texture(uTexNext, vUv);
+  // gl_FragColor = tex1;
+  // gl_FragColor = tex2;
+
+  gl_FragColor = mix(texCurrent, texNext, uProgress) ;
+  }
