8293a1a8b706ae3d525ac322c378c9de5fd6dc3f
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-04 10:30:44 +0900

    chore:#03 現在と次のtextureをsampler2Dで定義。

--- src/slider0/fragment.glsl
+++ src/slider0/fragment.glsl
@@ -4,7 +4,8 @@ precision mediump float;
 #pragma glslify: noise3 = require(glsl-noise/simplex/3d);
 
 varying vec2 vUv;
-uniform sampler2D uTex;
+uniform sampler2D uTexCurrent;
+uniform sampler2D uTexNext;
 uniform float uTick;
 uniform vec2 uNoiseScale;
 
@@ -13,6 +14,7 @@ void main() {
   // ヨコシマのノイズ
    float n = noise3(vec3(vUv.x * uNoiseScale.x, vUv.y * uNoiseScale.y, uTick * 0.01));
 
-  vec4 tex = texture(uTex, vUv + n);
+  vec4 tex1 = texture(uTexCurrent, vUv);
+  vec4 tex2 = texture(uTexNext, vUv);
   gl_FragColor = tex;
 }
