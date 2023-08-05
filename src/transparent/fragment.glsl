60e088f676be2a79230288323f8c90f85776d551
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-05 15:19:52 +0900

    fix:#03 THREE.WebGLProgram: Shader Error 0 - VALIDATE_STATUS false
    Program Info Log: Fragment shader is not compiled.

--- src/transparent/fragment.glsl
+++ src/transparent/fragment.glsl
@@ -6,7 +6,6 @@ uniform sampler2D uTex2;
 uniform float uTick;
 uniform float uProgress;
 
-
 void main() {
 
   vec4 color1 = texture2D(uTex1, vUv);
@@ -14,11 +13,12 @@ void main() {
 
 // Calculate the y-coordinate with respect to uProgress
   float yCoordinate = vUv.y * (1.0 - uProgress);
-  
+
   // Use the calculated y-coordinate to make color1 gradually transparent from bottom
-  gl_FragColor = mix(color1, vec4(0.0), smoothstep(0.0, 0.4, yCoordinate));
+  vec4 fadedColor1 = mix(color1, vec4(0.0), smoothstep(0.0, 0.4, yCoordinate));
 
+// Use uProgress to transition from gray to fadedColor1
+  vec4 finalColor = mix(toGrayscale(color1), fadedColor1, uProgress);
 
-  
-  // gl_FragColor = color1;
+  gl_FragColor = finalColor;
 }
