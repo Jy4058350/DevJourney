e5337520fef491f0f7a937c399f9269ef2ec8e11
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-13 07:32:29 +0900

    chore:#01 テクスチャのy座標をuProgressの値に応じ て変化させて、そのy座標をつかってテクスチャを取得する

--- src/yCoodinate/fragment.glsl
+++ src/yCoodinate/fragment.glsl
@@ -9,11 +9,14 @@ void main() {
 
   vec2 uv = vUv;
 
-  vec2 yCoodinate = (1 - uProgress) * uv;
+//テクスチャのy座標をuProgressの値に応じて変化させる
+  // vec2 yCoodinate = (1 - uProgress) * uv;
+  vec2 yCoodinate = vec2(uv.x, (1.0 - uProgress) * uv.y);
 
   // vec4 color = texture2D(uTex1, uv);
-vec4 yCoodinateColor = texture2D(uTex1, yCoodinate);
-  gl_FragColor = color;
+  //変更したy座標をつかってテクスチャを取得
+  vec4 yCoodinateColor = texture2D(uTex1, yCoodinate);
+  // gl_FragColor = color;
   gl_FragColor = yCoodinateColor;
 
 }
