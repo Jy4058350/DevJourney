657ec5cf9e058134e2093dd5de6e69db0c2e60c0
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-15 08:44:21 +0900

    feat:#07 平行移動を遅延なくできるコードをgchatに聞いて貼り付けました

--- src/vertexedit/vertex.glsl
+++ src/vertexedit/vertex.glsl
@@ -1,3 +1,4 @@
+
 varying vec2 vUv;
 varying float vExpandTime;
 attribute float aExpandTime;
@@ -8,7 +9,17 @@ void main() {
     vExpandTime = aExpandTime;
     vec3 pos = position;
 
-    float progress = clamp(uProgress * 1.3 - aExpandTime * 0.3, 0.0, 1.0);
-    pos.z += progress * -10.0;
+    // 調整可能な遅延係数
+    float delayFactor = 0.01; // 遅延係数を調整してください
+
+    // uProgress を速くすることで遅延を減少させる
+    float fastProgress = uProgress * 1.1;
+
+    // 遅延を調整した progress を計算
+    float adjustedProgress = clamp(fastProgress * (1.0 + aExpandTime * delayFactor), 0.0, 1.0);
+
+    // 調整した progress に基づいて位置を変更
+    pos.z += adjustedProgress * -10.0;
+
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
