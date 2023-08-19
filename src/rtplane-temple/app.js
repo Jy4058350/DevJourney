d9075c980909acb41ce3af88a183106884234dec
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-19 21:37:44 +0900

    chore:#03 meshとrtmeshのサイズ比較を計算してみた

--- src/rtplane-temple/app.js
+++ src/rtplane-temple/app.js
@@ -56,22 +56,22 @@ async function init() {
   const rtScene = new Scene();
   world.scene = new Scene();
 
-  rtCamera.position.set(0, 0, 5);
-  world.camera.position.set(0, 0, 5);
+  rtCamera.position.set(0, 0, 40);
+  world.camera.position.set(0, 0, 40);
 
   const controls = new OrbitControls(world.camera, world.renderer.domElement);
   controls.enableDamping = true;
 
-  const rtGeo = new PlaneGeometry(4, 4);
+  const rtGeo = new PlaneGeometry(40, 40);
   const rtMate = new THREE.MeshBasicMaterial({
     color: 0x009dff,
     side: DoubleSide,
   });
   const rtMesh = new Mesh(rtGeo, rtMate);
 
-  const geo = new PlaneGeometry(4, 4);
+  const geo = new PlaneGeometry(40, 40);
   const mate = new THREE.MeshBasicMaterial({
-    // color: 0x009dff,
+    color: 0x009dff,
     side: DoubleSide,
     map: renderTarget.texture,
   });
@@ -80,19 +80,19 @@ async function init() {
 
   world.scene.background = new Color(0xffffff);
 
-  const light = new PointLight(0xffffff, 10, 1000);
-  light.position.set(100, 100, 100);
-  const pHelper = new PointLightHelper(light);
-  world.scene.add(light, pHelper);
-  rtScene.add(light, pHelper);
-  const light1 = new PointLight(0xffffff, 100, 1000);
-  light.position.set(0, 0, 0);
-  const pHelper1 = new PointLightHelper(light1);
-  world.scene.add(light1, pHelper1);
-  rtScene.add(light1, pHelper1);
-  const light2 = new PointLight(0xffffff, 100, 1000);
-  light.position.set(-100, -100, -100);
-  rtScene.add(light2);
+  // const light = new PointLight(0xffffff, 10, 1000);
+  // light.position.set(100, 100, 100);
+  // const pHelper = new PointLightHelper(light);
+  // world.scene.add(light, pHelper);
+  // rtScene.add(light, pHelper);
+  // const light1 = new PointLight(0xffffff, 100, 1000);
+  // light.position.set(0, 0, 0);
+  // const pHelper1 = new PointLightHelper(light1);
+  // world.scene.add(light1, pHelper1);
+  // rtScene.add(light1, pHelper1);
+  // const light2 = new PointLight(0xffffff, 100, 1000);
+  // light.position.set(-100, -100, -100);
+  // rtScene.add(light2);
 
   rtScene.add(rtMesh);
 
@@ -104,6 +104,49 @@ async function init() {
     return texture;
   }
 
+  console.log(
+    "Mesh 1 Size:",
+    mesh.geometry.parameters.width,
+    mesh.geometry.parameters.height,
+    mesh.geometry.parameters.depth
+  );
+  console.log(
+    "Mesh 2 Size:",
+    rtMesh.geometry.parameters.width,
+    rtMesh.geometry.parameters.height,
+    rtMesh.geometry.parameters.depth
+  );
+
+  // カメラの視野角とビューポートのアスペクト比を取得
+  const fov = world.camera.fov * (Math.PI / 180); // ラジアンに変換
+  const aspect = world.camera.aspect;
+
+  // カメラからメッシュまでの距離を計算
+  const distance = world.camera.position.distanceTo(mesh.position);
+
+  // メッシュのスクリーン上での表示サイズを計算
+  const height = 2 * Math.tan(fov / 2) * distance;
+  const width = height * aspect;
+
+  console.log("Mesh Size on Screen:", width, height);
+  
+
+// カメラの視野角とビューポートのアスペクト比を取得
+const fov1 = rtCamera.fov * (Math.PI / 180); // ラジアンに変換
+const aspect1 = rtCamera.aspect;
+
+// カメラからメッシュまでの距離を計算
+const distance1 = rtCamera.position.distanceTo(rtMesh.position);
+
+// メッシュのスクリーン上での表示サイズを計算
+const height1 = 2 * Math.tan(fov1 / 2) * distance1;
+const width1 = height1 * aspect1;
+
+console.log("rtMesh Size on Screen:", width1, height1);
+
+
+
+
   let i = 0;
   function animate() {
     requestAnimationFrame(animate);
@@ -114,6 +157,7 @@ async function init() {
 
     // rtMesh.rotation.x += 0.01;
     // rtMesh.rotation.y += 0.01;
+    // rtMesh.position.y += 0.01;
 
     world.renderer.render(world.scene, world.camera);
     controls.update();
