0172f3ae339a61faba16840f8ffd9c515dc7c6d3
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-19 18:12:05 +0900

    revert:#01 やりなおし

--- src/rtplane-temple/app.js
+++ src/rtplane-temple/app.js
@@ -12,11 +12,8 @@ import {
   ShaderMaterial,
   Mesh,
   AxesHelper,
-  BoxGeometry,
-  DoubleSide,
-  WebGLRenderTarget,
 } from "three";
-// import * as THREE from "three";
+import * as THREE from "three";
 import vertexShader from "./vertex.glsl";
 import fragmentShader from "./fragment.glsl";
 import GUI from "lil-gui";
@@ -28,7 +25,6 @@ const world = {};
 init();
 
 async function init() {
-  //メインレンダラー
   const canvas = iNode.qs("#canvas");
   const canvasRect = canvas.getBoundingClientRect();
   world.renderer = new WebGLRenderer({
@@ -45,29 +41,15 @@ async function init() {
     0.1,
     1000
   );
+  world.camera.position.z = 30;
 
-
-  //ターゲットレンダー
-  world.renderTarget = new WebGLRenderTarget();
-  world.rtCamera = world.camera.clone();
-  world.rtScene = new Scene();
-  world.rtCamera.position.z = 100;
-
-  const geo = new BoxGeometry(25, 25, 25);
-  const mate = new ShaderMaterial({
-    map: world.renderTarget.texture,
-    vertexShader,
-    fragmentShader,
-  });
-  const rtGeo = new BoxGeometry(25, 25, 25);
-  const rtMate = new ShaderMaterial({
+  const geometry = new THREE.BoxGeometry(4, 4, 4);
+  const material = new ShaderMaterial({
     vertexShader,
     fragmentShader,
   });
 
-  const rtMesh = new Mesh(rtGeo, rtMate);
-  const mesh = new Mesh(geo, mate);
-  world.rtScene.add(rtMesh);
+  const mesh = new Mesh(geometry, material);
   world.scene.add(mesh);
 
   async function loadTex(url) {
@@ -78,23 +60,12 @@ async function init() {
     return texture;
   }
 
-  const axis = new AxesHelper(100);
-  world.scene.add(axis);
-
-  const controls = new OrbitControls(world.camera, world.renderer.domElement);
-  controls.enableDamping = true;
-
-  const gui = new GUI();
-
   let i = 0;
   function animate() {
     requestAnimationFrame(animate);
-    controls.update();
-    // mesh.rotation.x += 0.01;
-    // mesh.rotation.y += 0.01;
-    world.renderer.setRenderTarget(world.renderTarget);
-    world.renderer.render(world.rtScene, world.rtCamera);
-    world.renderer.setRenderTarget(null);
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
 
     world.renderer.render(world.scene, world.camera);
   }
