00be14f8e217387d2a5744e936fccd981796a19a
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-20 04:40:58 +0900

    feat:#01 スケールをあわせるための計算式をupdate

---
+++ src/camera_pos/app.js
@@ -0,0 +1,110 @@
+/**
+ * Three.js
+ * https://threejs.org/
+ */
+import "./test.scss";
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera,
+  TextureLoader,
+  PlaneGeometry,
+  ShaderMaterial,
+  Mesh,
+  AxesHelper,
+} from "three";
+import * as THREE from "three";
+import vertexShader from "./vertex.glsl";
+import fragmentShader from "./fragment.glsl";
+import GUI from "lil-gui";
+import { gsap } from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { iNode } from "../iNode";
+
+const world = {};
+init();
+
+async function init() {
+  const canvas = iNode.qs("#canvas");
+  const canvasRect = canvas.getBoundingClientRect();
+  world.renderer = new WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  world.renderer.setSize(canvasRect.width, canvasRect.height, false);
+  world.renderer.setClearColor(0x000000, 0);
+
+  world.scene = new Scene();
+
+  const cameraWidth = canvasRect.width;
+  const cameraHeight = canvasRect.height;
+  const near = 1500;
+  const far = 4000;
+  const aspect = cameraWidth / cameraHeight;
+  const cameraZ = 2000;
+  const radian = Math.atan(cameraHeight / 2 / 2 / cameraZ);
+  const fov = radian * (180 / Math.PI);
+
+  world.camera = new PerspectiveCamera(fov, aspect, near, far);
+  world.camera.position.z = cameraZ;
+
+  const geometry = new PlaneGeometry(128, 72);
+  const material = new ShaderMaterial({
+    uniforms: {
+      uTex1: { value: await loadTex("/img/output3.jpg") },
+      uTex2: { value: await loadTex("/img/output2.jpg") },
+      uTick: { value: 0 },
+      uProgress: { value: 0 },
+    },
+    vertexShader,
+    fragmentShader,
+  });
+
+  const mesh = new Mesh(geometry, material);
+  world.scene.add(mesh);
+
+  async function loadTex(url) {
+    const texLoader = new TextureLoader();
+    const texture = await texLoader.loadAsync(url);
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.MirroredRepeatWrapping;
+    return texture;
+  }
+
+  const axis = new AxesHelper(100);
+  world.scene.add(axis);
+
+  const controls = new OrbitControls(world.camera, world.renderer.domElement);
+  controls.enableDamping = true;
+
+  const gui = new GUI();
+  const folder1 = gui.addFolder("");
+  folder1.open();
+
+  folder1
+    .add(material.uniforms.uProgress, "value", 0, 1, 0.1)
+    .name("")
+    .listen();
+
+  const datData = { next: !!material.uniforms.uProgress.value };
+  folder1
+    .add(datData, "next")
+    .name("")
+    .onChange(() => {
+      gsap.to(material.uniforms.uProgress, {
+        value: datData.next ? 1 : 0,
+        duration: 3,
+        ease: "ease",
+      });
+    });
+
+  let i = 0;
+  function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+
+    world.renderer.render(world.scene, world.camera);
+  }
+
+  animate();
+}
