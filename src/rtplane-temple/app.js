c0030b0273e9cf59deefbbcf66ddc7ea72161004
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-19 19:22:39 +0900

    chore:#01 renderの模擬の一区切り。worldオブジェクトにレンダーターゲットをつけるとうまくいかない。メインのみworldをつける

--- src/rtplane-temple/app.js
+++ src/rtplane-temple/app.js
@@ -12,6 +12,12 @@ import {
   ShaderMaterial,
   Mesh,
   AxesHelper,
+  BoxGeometry,
+  MeshLambertMaterial,
+  DoubleSide,
+  Color,
+  PointLight,
+  WebGLRenderTarget,
 } from "three";
 import * as THREE from "three";
 import vertexShader from "./vertex.glsl";
@@ -34,23 +40,57 @@ async function init() {
   world.renderer.setSize(canvasRect.width, canvasRect.height, false);
   world.renderer.setClearColor(0x000000, 0);
 
-  world.scene = new Scene();
+
+//レンダーターゲット
+const renderTarget = new WebGLRenderTarget(500, 500);
+
+
   world.camera = new PerspectiveCamera(
     75,
     canvasRect.width / canvasRect.height,
     0.1,
     1000
   );
-  world.camera.position.z = 30;
+  const rtCamera = world.camera.clone();
+  rtCamera.aspect = 1;
+  const rtScene = new Scene();
+  world.scene = new Scene();
 
-  const geometry = new THREE.BoxGeometry(4, 4, 4);
-  const material = new ShaderMaterial({
-    vertexShader,
-    fragmentShader,
+  world.camera.position.set(-10, -10, 10);
+
+  const controls = new OrbitControls(world.camera, world.renderer.domElement);
+  controls.enableDamping = true;
+
+  const rtGeo = new BoxGeometry(4, 4, 4);
+  const rtMate = new MeshLambertMaterial({
+    color: 0x009dff,
+    side: DoubleSide,
   });
+  const rtMesh = new Mesh(rtGeo, rtMate);
 
-  const mesh = new Mesh(geometry, material);
+  const geo = new BoxGeometry(4, 4, 4);
+  const mate = new THREE.MeshBasicMaterial({
+    color: 0x009dff,
+    side: DoubleSide,
+    map: renderTarget.texture,
+  });
+  const mesh = new Mesh(geo, mate);
   world.scene.add(mesh);
+
+
+ 
+  world.scene.background = new Color(0xeeeeee);
+  const light = new PointLight(0xffffff, 1, 0);
+  light.position.set(10, 20, 10);
+  rtScene.add(light);
+  const light1 = new PointLight(0xffffff, 1, 0);
+  light.position.set(10, 10, 10);
+  rtScene.add(light1);
+  const light2 = new PointLight(0xffffff, 1, 0);
+  light.position.set(-10, -20, -10);
+  rtScene.add(light2);
+
+  rtScene.add(rtMesh);
 
   async function loadTex(url) {
     const texLoader = new TextureLoader();
@@ -64,10 +104,15 @@ async function init() {
   function animate() {
     requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
+    world.renderer.setRenderTarget(renderTarget);
+    world.renderer.render(rtScene, rtCamera);
+    world.renderer.setRenderTarget(null);
+
+    rtMesh.rotation.x += 0.01;
+    rtMesh.rotation.y += 0.01;
 
     world.renderer.render(world.scene, world.camera);
+    controls.update();
   }
 
   animate();
--- src/rtplane-temple/fragment.glsl
+++ src/rtplane-temple/fragment.glsl
@@ -4,6 +4,6 @@ varying vec2 vUv;
 
 void main() {
   vec2 v1 = vec2(0.0);
-  gl_FragColor = vec4(0.5, 0.3, 0.9, 1.0);
+  gl_FragColor = vec4(vUv, 0.9, 1.0);
 
 }
