7415292fdaa6179a5f24566e7548ff099b9c680f
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-21 19:46:43 +0900

    chore:#04 qsaに変更を始める

--- src/slider1/app.js
+++ src/slider1/app.js
@@ -127,73 +127,76 @@ async function init() {
     return texture;
   }
 
-  //   const hovered = iNode.qsa(".hovered");
-  //   const openSubmenu = iNode.qs(".open-submenu");
-  //   const header = iNode.qs(".header");
-  //   const dev = iNode.qs(".dev");
-  //   const account = iNode.qs(".account");
+  function getWorldPosition(rect, canvasRect) {
+    const x = rect.left + rect.width / 2 - canvasRect.width / 2;
+    const y = -rect.top - rect.height / 2 + canvasRect.height / 2;
+    return { x, y };
+  }
 
-  //   hovered.forEach((item) => {
-  //     item.addEventListener("mouseenter", () => {
-  //       item.classList.add("active");
-  //       openSubmenu.classList.add("active");
-  //       header.classList.add("white");
-  //       dev.classList.add("white");
-  //       account.classList.add("white");
-  //       hovered.forEach((item) => {
-  //         item.classList.add("white");
-  //       });
-  //     });
-  //   });
 
-  //   hovered.forEach((item) => {
-  //     item.addEventListener("mouseleave", () => {
-  //       if (!openSubmenu.matches(":hover")) {
-  //         item.classList.remove("active");
-  //         openSubmenu.classList.remove("active");
-  //         // header.classList.remove("white");
-  //         // dev.classList.remove("white");
-  //         account.classList.remove("white");
-  //         // hovered.forEach((item) => {
-  //         //   item.classList.remove("white");
-  //         // });
-  //       }
-  //     });
-  //   });
-  //   openSubmenu.addEventListener("mouseleave", () => {
-  //     if (!openSubmenu.matches(":hover")) {
-  //       openSubmenu.classList.remove("active");
-  //       header.classList.remove("white");
-  //       dev.classList.remove("white");
-  //       account.classList.remove("white");
-  //       hovered.forEach((item) => {
-  //         item.classList.remove("white");
-  //       });
-  //     }
-  //   });
+    const hovered = iNode.qsa(".hovered");
+    const openSubmenu = iNode.qs(".open-submenu");
+    const header = iNode.qs(".header");
+    const dev = iNode.qs(".dev");
+    const account = iNode.qs(".account");
 
-  //   header.addEventListener("mouseenter", () => {
-  //     dev.classList.add("white");
-  //     header.classList.add("white");
-  //     hovered.forEach((item) => {
-  //       item.classList.add("white");
-  //     });
-  //   });
-  //   header.addEventListener("mouseleave", () => {
-  //     if (!openSubmenu.matches(".active")) {
-  //       header.classList.remove("white");
-  //       dev.classList.remove("white");
-  //       hovered.forEach((item) => {
-  //         item.classList.remove("white");
-  //       });
-  //     }
-  //     if (!openSubmenu.matches(".active")) {
-  //       header.classList.remove("white");
-  //     }
-  //   });
+    hovered.forEach((item) => {
+      item.addEventListener("mouseenter", () => {
+        item.classList.add("active");
+        openSubmenu.classList.add("active");
+        header.classList.add("white");
+        dev.classList.add("white");
+        account.classList.add("white");
+        hovered.forEach((item) => {
+          item.classList.add("white");
+        });
+      });
+    });
+
+    hovered.forEach((item) => {
+      item.addEventListener("mouseleave", () => {
+        if (!openSubmenu.matches(":hover")) {
+          item.classList.remove("active");
+          openSubmenu.classList.remove("active");
+          // header.classList.remove("white");
+          // dev.classList.remove("white");
+          account.classList.remove("white");
+          // hovered.forEach((item) => {
+          //   item.classList.remove("white");
+          // });
+        }
+      });
+    });
+    openSubmenu.addEventListener("mouseleave", () => {
+      if (!openSubmenu.matches(":hover")) {
+        openSubmenu.classList.remove("active");
+        header.classList.remove("white");
+        dev.classList.remove("white");
+        account.classList.remove("white");
+        hovered.forEach((item) => {
+          item.classList.remove("white");
+        });
+      }
+    });
+
+    header.addEventListener("mouseenter", () => {
+      dev.classList.add("white");
+      header.classList.add("white");
+      hovered.forEach((item) => {
+        item.classList.add("white");
+      });
+    });
+    header.addEventListener("mouseleave", () => {
+      if (!openSubmenu.matches(".active")) {
+        header.classList.remove("white");
+        dev.classList.remove("white");
+        hovered.forEach((item) => {
+          item.classList.remove("white");
+        });
+      }
+      if (!openSubmenu.matches(".active")) {
+        header.classList.remove("white");
+      }
+    });
 }
-function getWorldPosition(rect, canvasRect) {
-  const x = rect.left + rect.width / 2 - canvasRect.width / 2;
-  const y = -rect.top - rect.height / 2 + canvasRect.height / 2;
-  return { x, y };
-}
+
