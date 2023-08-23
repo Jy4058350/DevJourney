603724e1d26572fee5d00b5f6dd2675890130c3b
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-23 11:44:00 +0900

    resize()　create

---
+++ src/temple_resize/app.js
@@ -0,0 +1,184 @@
+/**
+ * Three.js
+ * https://threejs.org/
+ */
+import "./test.scss";
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera,
+  TextureLoader,
+  PlaneGeometry,
+  ShaderMaterial,
+  Mesh,
+  AxesHelper,
+  ClampToEdgeWrapping,
+  MirroredRepeatWrapping,
+} from "three";
+import vertexShader from "./vertex.glsl";
+import fragmentShader from "./fragment.glsl";
+import GUI from "lil-gui";
+import { gsap } from "gsap";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { iNode } from "../iNode";
+
+const world = {};
+const os = [];
+const canvas = iNode.qs("#canvas");
+const canvasRect = canvas.getBoundingClientRect();
+init();
+
+async function init() {
+  world.renderer = new WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  world.renderer.setSize(canvasRect.width, canvasRect.height, false);
+  world.renderer.setClearColor(0x000000, 0);
+
+  const cameraWidth = canvasRect.width;
+  const cameraHeight = canvasRect.height;
+  const near = 1500;
+  const far = 4000;
+  const aspect = cameraWidth / cameraHeight;
+  const cameraZ = 2000;
+  const radian = 2 * Math.atan(cameraHeight / 2 / cameraZ);
+  const fov = radian * (180 / Math.PI);
+
+  world.scene = new Scene();
+  world.camera = new PerspectiveCamera(
+    fov,
+    cameraWidth / cameraHeight,
+    near,
+    far
+  );
+  world.camera.position.z = cameraZ;
+
+  const axis = new AxesHelper(100);
+  world.scene.add(axis);
+
+  const controls = new OrbitControls(world.camera, world.renderer.domElement);
+  controls.enableDamping = true;
+
+  const els = iNode.qsa("[data-webgl]");
+  els.forEach(async (el) => {
+    const rect = el.getBoundingClientRect();
+    const geometry = new PlaneGeometry(rect.width, rect.height, 1, 1);
+    const material = new ShaderMaterial({
+      uniforms: {
+        uTex: { value: await loadTex("img/output2.jpg") },
+        uTick: { value: 0 },
+        uProgress: { value: 0 },
+      },
+      vertexShader,
+      fragmentShader,
+    });
+
+    const mesh = new Mesh(geometry, material);
+    world.scene.add(mesh);
+
+    initResize();
+
+    const { x, y } = getWorldPosition(rect, canvasRect);
+    mesh.position.set(x, y, 0);
+
+    const o = {
+      $: { el },
+      mesh,
+      geometry,
+      material,
+      rect,
+      canvasRect,
+    };
+    os.push(o);
+
+    const gui = new GUI();
+    const folder1 = gui.addFolder("");
+    folder1.open();
+
+    folder1
+      .add(material.uniforms.uProgress, "value", 0, 1, 0.1)
+      .name("")
+      .listen();
+
+    const datData = { next: !!material.uniforms.uProgress.value };
+    folder1
+      .add(datData, "next")
+      .name("")
+      .onChange(() => {
+        gsap.to(material.uniforms.uProgress, {
+          value: datData.next ? 1 : 0,
+          duration: 3,
+          ease: "ease",
+        });
+      });
+  });
+
+  let i = 0;
+  function animate() {
+    requestAnimationFrame(animate);
+    os.forEach((o) => scroll(o)); //この記述を覚える！！
+    controls.update();
+    world.renderer.render(world.scene, world.camera);
+  }
+  animate();
+}
+
+async function loadTex(url) {
+  const texLoader = new TextureLoader();
+  const texture = await texLoader.loadAsync(url);
+  texture.wrapS = ClampToEdgeWrapping;
+  texture.wrapT = MirroredRepeatWrapping;
+  return texture;
+}
+
+function getWorldPosition(rect, canvasRect) {
+  const x = rect.left + rect.width / 2 - canvasRect.width / 2;
+  const y = -rect.top - rect.height / 2 + canvasRect.height / 2;
+  return { x, y };
+}
+
+function scroll(o) {
+  const {
+    $: { el },
+    mesh,
+  } = o;
+  const rect = el.getBoundingClientRect();
+  const { y } = getWorldPosition(rect, canvasRect);
+  mesh.position.y = y;
+}
+
+function resize(o, newCanvasRect) {
+  const {
+    $: { el },
+    mesh,
+  } = o;
+  const rect = el.getBoundingClientRect();
+  console.log(rect.width, rect.height);
+  const { x, y } = getWorldPosition(rect, newCanvasRect);
+  mesh.position.set(x, y, 0);
+
+  //大きさの変更
+
+}
+
+function initResize() {
+  let timer = 0;
+  window.addEventListener("resize", () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      console.log("resize");
+//canvasサイズの変更
+const newCanvasRect = canvas.getBoundingClientRect();
+console.log(newCanvasRect.width,newCanvasRect.height)
+world.renderer.setSize(newCanvasRect.width,newCanvasRect.height,false);
+
+//meshの位置とサイズの変更
+os.forEach((o) => resize(o, newCanvasRect));
+
+// cameraのProjectionMatrixの更新
+
+
+    }, 500);
+  });
+}
