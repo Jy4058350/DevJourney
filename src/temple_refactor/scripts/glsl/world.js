0b7fbe0e4e83862d44b1fc53ca0f60f6325c42a3
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-25 14:47:42 +0900

    chore:#02 canvasはnewになったりして変化してました。その場合にはdom可変要素として管理するため＄変数にいれておくほうが制御しやすいようです

--- src/temple_refactor/scripts/glsl/world.js
+++ src/temple_refactor/scripts/glsl/world.js
@@ -1,11 +1,55 @@
+import { WebGLRenderer } from "three";
+
+import viewport from "../helper/viewport";
+
 const world = {
-    os: [],
-    init,
+  os: [],
+  init,
+};
+
+function init(canvas, viewport) {
+  world.renderer = new WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  world.renderer.setSize(viewport.width, viewport.height, false);
+  world.renderer.setClearColor(0x000000, 0);
+  world.renderer.setPixelRatio(viewport.devicePixelRatio);
+
+  world.scene = new Scene();
+
+  world.camera = _setupPerspectiveCamera(viewport);
 }
 
-function init() {
-
+function _setupPerspectiveCamera(viewport) {
+  const { fov, near, far, cameraZ } = viewport;
+  const camera = new PerspectiveCamera(
+    fov,
+    cameraWidth / cameraHeight,
+    near,
+    far
+  );
+  world.camera.position.z = cameraZ;
+  return camera;
 }
 
+function fitWorldPositon(viewport) {
+  world.renderer.setSize(viewport.width, viewport.height, false);
+
+  // meshの位置と大きさの変更
+  os.forEach((o) => resize(o, newCanvasRect));
+
+  updateCamera(viewport);
+}
+
+function updateCamera(viewport) {
+  const { fov, near, far, cameraZ } = viewport;
+  world.camera.near = near;
+  world.camera.far = far;
+  world.camera.aspect = aspect;
+  world.camera.fov = fov;
+  world.camera.updateProjectionMatrix();
+  return world.camera;
+}
 
 export default world;
---
+++ src/temple_refactor/scripts/helper/viewport.js
@@ -0,0 +1,46 @@
+import world from "../glsl/world";
+
+const viewport = {
+  init,
+};
+
+const $ = {};
+
+function init(canvas, cameraZ = 2000, near = 1500, far = 4000) {
+  $.canvas = canvas;
+  const rect = canvas.getBoundingClientRect();
+
+  viewport.width = rect.width;
+  viewport.height = rect.height;
+  viewport.near = near;
+  viewport.far = far;
+  viewport.aspect = viewport.width / viewport.height;
+  viewport.cameraZ = cameraZ;
+  viewport.rad = 2 * Math.atan(viewport.height / 2 / cameraZ);
+  viewport.fov = viewport.rad * (180 / Math.PI);
+  viewport.devicePixelRatio = window.devicePixelRatio;
+
+  _initResize();
+  return viewport;
+}
+
+function _update() {
+  const { cameraZ, near, far } = viewport;
+  viewport.init($.canvas, cameraZ, near, far);
+}
+
+function _initResize() {
+  let timer = null;
+  window.addEventListener("resize", () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      _onResize();
+    }, 500);
+  });
+}
+
+function _onResize() {
+  _update();
+}
+
+export default viewport;
