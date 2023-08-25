5049d850c56b99dfc4f56691faf317a084ec035d
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-25 18:17:25 +0900

    chore:#02 ゆらぎの速さと強度をfloatで変数として取り出しました

--- src/try_yuragi/vertex.glsl
+++ src/try_yuragi/vertex.glsl
@@ -12,6 +12,9 @@ void main() {
     // float progress =uProgress;
     float progress = clamp(uProgress * 1.3 - aDelay * 0.3, 0.0, 1.0);
     // float progress = sin(uProgress*1.3 - aDelay * 0.3);
-    pos.z = sin(progress * 6.28 * 9.0) * 20.0;
+
+    float a = 20.0;//揺らぎの速さです。サインカーブの周期が増えます。
+    float b = 20.0;//揺らぎの大きさです。サインカーブの振幅が増えます。
+    pos.z = sin(progress * 3.14 * a) * b;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
