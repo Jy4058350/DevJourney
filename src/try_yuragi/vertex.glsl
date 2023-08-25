bbfde95562346e6f585a50c5a01192ba0eccbc84
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-26 06:20:24 +0900

    fix:#02 setupGeometryに新しい計算式を挑戦したけど、うまくいかなかった。がしかし消さずにそのまま残して次の課題とする

--- src/try_yuragi/vertex.glsl
+++ src/try_yuragi/vertex.glsl
@@ -8,13 +8,19 @@ attribute float aDelay;
 void main() {
     vUv = uv;
     vDelay = aDelay;
+
     vec3 pos = position;
     // float progress =uProgress;
-    float progress = clamp(uProgress * 1.3 - aDelay * 0.3, 0.0, 1.0);
+    float c = sin(aDelay * tan(aDelay * 1.4));
+    float progress = clamp(uProgress * 1.3 - c * 0.3, 0.0, 1.0);
     // float progress = sin(uProgress*1.3 - aDelay * 0.3);
+    float fpro = progress;
 
-    float a = 20.0;//揺らぎの速さです。サインカーブの周期が増えます。
-    float b = 20.0;//揺らぎの大きさです。サインカーブの振幅が増えます。
-    pos.z = sin(progress * 3.14 * a) * b;
+    float a = 5.0;//揺らぎの速さです。サインカーブの周期が増えます。
+    float b = 60.0;//揺らぎの大きさです。サインカーブの振幅が増えます。
+    pos.x += sin(progress * 3.14) * 20.0;
+    // pos.x += sin((progress, 0.1, 0.4) * 3.14) * 20.0;
+    pos.y += sin(progress * 3.14) * 50.0;
+    pos.z = cos(progress * 3.14 * a) * b;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
