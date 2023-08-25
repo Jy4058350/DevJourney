b2cad1360bb05cb2fc0551fb363cb162e0c1817a
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-08-25 18:10:16 +0900

    chore:#01 ゆらめく布見ないな感じ。

--- src/try_yuragi/vertex.glsl
+++ src/try_yuragi/vertex.glsl
@@ -12,6 +12,6 @@ void main() {
     // float progress =uProgress;
     float progress = clamp(uProgress * 1.3 - aDelay * 0.3, 0.0, 1.0);
     // float progress = sin(uProgress*1.3 - aDelay * 0.3);
-    pos.z = sin(progress * 3.14) * 400.0;
+    pos.z = sin(progress * 6.28 * 9.0) * 20.0;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
