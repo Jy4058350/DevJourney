a81fbe592e0c59b6f3de5b1b4ebfe82a79025a88
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-01 05:35:19 +0900

    chore:#04 if文の実装確認ができた。上からカウントしている。斜めになる。問題なのは斜めになること。これはtexture2DでvUvをかけているから

--- src/vertexedit3/fragment.glsl
+++ src/vertexedit3/fragment.glsl
@@ -8,15 +8,24 @@ varying vec2 vUv;
 
 void main() {
 
-    float v = vNormalizedValue - 0.5;
+    float v = vNormalizedValue;
+
+    if(v < 0.5) {
+        v = 0.0;
+    } else {
+        v = 1.0;
+    }
+  
+
+
 
     vec4 tex = texture2D(uTex, vUv);
 
-    gl_FragColor = tex;
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = tex * v;
 
-    gl_FragColor = vec4(1.0 * v, 0.0, 0.0, 1.0);
-}
+
 
 //    if(distance(gl_PointCoord, vec2(0.5, 0.5)) > 0.7) {
 //     discard;
-//   }
+  }
