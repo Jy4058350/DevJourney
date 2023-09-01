e81c595ea2bb7d58bde59c1f69a654e476e66255
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-01 17:56:52 +0900

    first

---
+++ src/temple_refactor2/scripts/fragment.glsl
@@ -0,0 +1,20 @@
+precision mediump float;
+
+varying vec2 vUv;
+uniform sampler2D uTex1;
+uniform float uTick;
+uniform float uProgress;
+uniform vec2 uMouse;
+uniform float uHover;
+
+void main() {
+
+  vec2 uv = vUv;
+  vec4 color = texture2D(uTex1, uv);
+
+  vec2 mouse = step(uMouse, uv);
+
+  gl_FragColor = vec4(mouse, uHover, 1.0);
+  gl_FragColor = vec4(1.0, 0.5, 1, 1.0);
+
+}
