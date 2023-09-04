a3c105cd479e549366a51d1565cb24ab103a4e00
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-04 17:24:05 +0900

    feat:#03 cashに含まれない場合＝＞hasをつかうことができる

--- src/temple_refactor3_tex2/scripts/component/cash.js
+++ src/temple_refactor3_tex2/scripts/component/cash.js
@@ -3,25 +3,27 @@ import { iNode } from "../../../iNode";
 const cash = {
   init,
 };
-const cashes = [];
+const cashes = new Map();
 
 function init() {
   const els = iNode.qsa("[data-webgl]");
-  console.log(els);
-
-
+  //   console.log(els);
 
   els.forEach((el) => {
     const data = el.dataset;
     console.log(data);
-    for(let key in data) {
-        if(!key.startsWith("tex")) continue;
-        key = key.replace("-", "");
-        console.log(key);
-        cashes.push(key);
+    for (let key in data) {
+      if (!key.startsWith("tex")) continue;
+      const url = data[key];
+      key = key.replace("-", "");
+      console.log(key);
+      console.log(url);
+      if (!cashes.has(key)) {
+        cashes.set(key, url);
+      }
     }
-});
-console.log(cashes);
+  });
+  console.log(cashes);
 }
 
 export default cash;
