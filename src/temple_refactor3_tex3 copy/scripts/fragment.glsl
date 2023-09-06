17724655bb0df70726c18143d758e5f9dd42083d
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-07 06:24:55 +0900

    feat:01 単純に拡大縮小はuResolutionの係数を掛け合わせたらできた。

--- src/temple_refactor3_tex3 copy/scripts/fragment.glsl
+++ src/temple_refactor3_tex3 copy/scripts/fragment.glsl
@@ -5,10 +5,9 @@ uniform sampler2D tex1;
 uniform sampler2D tex2;
 
 uniform vec4 uResolution;
-vec2 coverUv(vec2 uv , vec4 uResolution) {
-return (uv - .5) * uResolution.zw + .5;
+vec2 coverUv(vec2 uv, vec4 uResolution) {
+  return (uv - .5) * uResolution.zw*0.9 + .5;
 }
-
 
 uniform vec4 uVClamp;
 // vec2 clampUv(vec2 uv, vec4 uVClamp) {
@@ -19,20 +18,19 @@ uniform vec4 uVClamp;
 void main() {
 
   // vec2 uv = vUv;
-vec2 uvb = coverUv(vUv, uResolution);
+  vec2 uvb = coverUv(vUv, uResolution);
 
 // vec2 uva = clampUv(vUv, uVClamp);
 // vec4 texColor3 = texture2D(tex1, uva);
-vec2 uvz = clamp(vUv, uVClamp.xy, uVClamp.zw);
+  vec2 uvz = clamp(vUv, uVClamp.xy, uVClamp.zw);
 
-vec4 texColor1 = texture2D(tex1, uvb);
-vec4 texColor11 = texture2D(tex1, vUv);
-vec4 texColor2 = texture2D(tex2, uvb);
+  vec4 texColor1 = texture2D(tex1, uvb);
+  vec4 texColor11 = texture2D(tex1, vUv);
+  vec4 texColor2 = texture2D(tex2, uvb);
 
-gl_FragColor = mix(texColor1, texColor2, smoothstep(0.3, 0.5, vUv.x));
-gl_FragColor = texColor11;
-gl_FragColor = texColor1;
+  gl_FragColor = mix(texColor1, texColor2, smoothstep(0.3, 0.5, vUv.x));
+  gl_FragColor = texColor11;
+  gl_FragColor = texColor1;
 // gl_FragColor = texture2D(tex1, uvz);
-
 
 }
