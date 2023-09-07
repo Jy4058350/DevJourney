2b068f0b3d43d62183cc87656143cdfa40d1049a
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-07 15:10:10 +0900

    test:#04 alphaをいじるとハレーションが際立ちすぎて風合いがよくない

--- src/yCoodinate_1/fragment.glsl
+++ src/yCoodinate_1/fragment.glsl
@@ -13,11 +13,20 @@ void main() {
   vec2 uv = vUv;
 
   vec4 tex1 = texture(uTex1, uv);
+  float tex1a = tex1.a * clamp(uProgress, 0.2, 1.0);
+  vec4 tex1aa = vec4(tex1.rgb, tex1a);
+
   vec4 tex2 = texture(uTex2, uv);
+  float tex2a = tex2.a * (1.0 - clamp(uProgress, 0.0, 0.6));
+  vec4 tex2aa = vec4(tex2.rgb, tex2a);
 
   vec4 tex3 = texture(uTex3, uv);//graytoon
-  tex3.a *= 0.5;
+  tex3.a *= 0.5;//透明度の設定
 
-  gl_FragColor = mix(tex1, tex3, step(uProgress, uv.y));
+  vec4 tex4 = mix(tex1aa, tex2aa, smoothstep(uProgress - 0.3, uProgress + 0.3, uv.y));
 
+  gl_FragColor = tex4;
+  // gl_FragColor = tex1;
+  // gl_FragColor = tex2aa;
+  // gl_FragColor = tex1aa;
 }
