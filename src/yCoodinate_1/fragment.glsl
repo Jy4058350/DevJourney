beb922b9ba6acbbc7b175d95b611eee404f07464
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-07 13:37:09 +0900

    test:#03 透明度を０．５かけたが、変化なし。裏側にtex1がいないようです

--- src/yCoodinate_1/fragment.glsl
+++ src/yCoodinate_1/fragment.glsl
@@ -16,7 +16,7 @@ void main() {
   vec4 tex2 = texture(uTex2, uv);
 
   vec4 tex3 = texture(uTex3, uv);//graytoon
-  tex3.a = tex3.a * 0.9;
+  tex3.a *= 0.5;
 
   gl_FragColor = mix(tex1, tex3, step(uProgress, uv.y));
 
