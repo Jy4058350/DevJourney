b34d5c67f9feb2ca484a9c6a9272a9dc08943d5f
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-09 18:55:10 +0900

    chore:#02 class表記にかえてメソッドを並べる

--- src/blur copy/buffergeo.js
+++ src/blur copy/buffergeo.js
@@ -1,19 +1,20 @@
 import { PlaneGeometry, BufferGeometry, BufferAttribute } from "three";
 
-function Setgeo(wSeg, hSeg) {
-  this.wSeg = wSeg;
-  this.hSeg = hSeg;
-  this.geometry = new BufferGeometry();
-  this.plane = new PlaneGeometry(50, 25, this.wSeg, this.hSeg);
+class Setgeo {
+  constructor(wSeg, hSeg) {
+    this.wSeg = wSeg;
+    this.hSeg = hSeg;
+    this.geometry = new BufferGeometry();
+    this.plane = new PlaneGeometry(50, 25, this.wSeg, this.hSeg);
+  }
+  createBufferGeo() {
+    const planeIndex = this.plane.getIndex().array;
+    const index = new BufferAttribute(planeIndex, 1);
+    this.geometry.setAttribute("position", this.plane.getAttribute("position"));
+    this.geometry.setAttribute("uv", this.plane.getAttribute("uv"));
+    this.geometry.setIndex(index);
+    return this.geometry;
+  }
 }
-
-Setgeo.prototype.createBufferGeo = function () {
-  const planeIndex = this.plane.getIndex().array;
-  const index = new BufferAttribute(planeIndex, 1);
-  this.geometry.setAttribute("position", this.plane.getAttribute("position"));
-  this.geometry.setAttribute("uv", this.plane.getAttribute("uv"));
-  this.geometry.setIndex(index);
-  return this.geometry;
-};
 
 export { Setgeo };
