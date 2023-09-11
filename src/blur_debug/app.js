55c7dd0fccd214a6d02f60ce6c35e5c8d2ac97c7
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-11 15:28:05 +0900

    chore:#02 raycast

--- src/blur_debug/app.js
+++ src/blur_debug/app.js
@@ -11,11 +11,17 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { iNode } from "../iNode";
 
 const world = {};
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Scrollbar from "smooth-scrollbar";
+const os = [];
+const canvas = iNode.qs("#canvas");
+const canvasRect = canvas.getBoundingClientRect();
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
 
 init();
 async function init() {
-  const canvas = iNode.qs("#canvas");
-  const canvasRect = canvas.getBoundingClientRect();
   world.renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
   world.renderer.setSize(canvasRect.width, canvasRect.height, false);
   world.renderer.setPixelRatio(window.devicePixelRatio);
@@ -34,14 +40,6 @@ async function init() {
   world.camera.position.z = cameraZ;
   // document.body.appendChild(world.renderer.domElement);
 
-  async function loadTex(url) {
-    const texLoader = new THREE.TextureLoader();
-    const texture = await texLoader.loadAsync(url);
-    texture.wrapS = THREE.ClampToEdgeWrapping;
-    texture.wrapT = THREE.MirroredRepeatWrapping;
-    return texture;
-  }
-
   // function Setgeo(w, h) {
   //   const geometry = new THREE.BufferGeometry();
   //   const plane = new THREE.PlaneGeometry(w, h, 1, 1);
@@ -57,18 +55,18 @@ async function init() {
   class Setgeo {
     constructor(w, h) {
       this.geometry = new THREE.BufferGeometry();
-      // console.log(w, h);
       this.plane = new THREE.PlaneGeometry(w, h, 1, 1);
     }
     createBufferGeo(w, h) {
       this.geometry.setAttribute(
         "position",
         this.plane.getAttribute("position")
-        );
-        this.geometry.setAttribute("uv", this.plane.getAttribute("uv"));
-        const planeIndex = this.plane.getIndex().array;
-        const index = new THREE.BufferAttribute(planeIndex, 1);
-        this.geometry.setIndex(index);
+      );
+      this.geometry.setAttribute("uv", this.plane.getAttribute("uv"));
+
+      const planeIndex = this.plane.getIndex().array;
+      const index = new THREE.BufferAttribute(planeIndex, 1);
+      this.geometry.setIndex(index);
       return this.geometry;
     }
   }
@@ -78,13 +76,15 @@ async function init() {
     const rect = el.getBoundingClientRect();
 
     const set = new Setgeo(rect.width, rect.height);
-    const geometry = set.createBufferGeo(rect.width, rect.height);
-    // const geometry = Setgeo(rect.width, rect.height);
+    const geometry = set.createBufferGeo(rect.width, rect.height, 1, 1);
+    // const geometry = new THREE.PlaneGeometry(rect.width, rect.height, 1, 1);
     const material = new THREE.ShaderMaterial({
       uniforms: {
-        uTex1: { value: await loadTex("/img/output3.jpg") },
-        uTex2: { value: await loadTex("/img/output2.jpg") },
+        // uTex1: { value: await loadTex("/img/output3.jpg") },
+        // uTex2: { value: await loadTex("/img/output2.jpg") },
         uTick: { value: 0 },
+        uHover: { value: 0 },
+        uMouse: { value: new THREE.Vector2(0.5, 0.5) },
         uProgress: { value: 0 },
       },
       vertexShader,
@@ -98,9 +98,23 @@ async function init() {
     mesh.position.x = x;
     mesh.position.y = y;
 
+    const o = {
+      rect,
+      mesh,
+      geometry,
+      material,
+      canvasRect,
+      $: {
+        el,
+      },
+    };
+    os.push(o);
+
     world.scene.add(mesh);
+    initResize();
   });
 
+  initScroll();
   // const axis = new THREE.AxesHelper(100);
   // world.scene.add(axis);
 
@@ -127,10 +141,13 @@ async function init() {
   //       ease: "ease",
   //     });
   //   });
-
-  animate();
-  function animate() {
-    requestAnimationFrame(animate);
+  rendere();
+  function rendere() {
+    requestAnimationFrame(rendere);
+    os.forEach((o) => {
+      scroll(o);
+    });
+    raycast();
     world.renderer.render(world.scene, world.camera);
     // controls.update();
   }
@@ -140,4 +157,152 @@ function getWorldPosition(rect, canvasRect) {
   const x = rect.left + rect.width / 2 - canvasRect.width / 2;
   const y = -rect.top - rect.height / 2 + canvasRect.height / 2;
   return { x, y };
+}
+
+function resize(o, newCanvasRect) {
+  const {
+    mesh,
+    rect,
+    geometry,
+    $: { el },
+  } = o;
+  const nextRect = el.getBoundingClientRect();
+  const { x, y } = getWorldPosition(nextRect, newCanvasRect);
+  mesh.position.x = x;
+  mesh.position.y = y;
+
+  geometry.scale(nextRect.width / rect.width, nextRect.height / rect.height, 1);
+  o.rect = nextRect;
+}
+
+function initResize() {
+  let timer = null;
+  window.addEventListener("resize", () => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      console.log("resize");
+      const newCanvasRect = canvas.getBoundingClientRect();
+      world.renderer.setSize(canvasRect.width, canvasRect.height, false);
+      os.forEach((o) => {
+        resize(o, newCanvasRect);
+      });
+
+      const cameraWidth = newCanvasRect.width;
+      const cameraHeight = newCanvasRect.height;
+      const near = 1500;
+      const far = 4000;
+      const aspect = cameraWidth / cameraHeight;
+      const cameraZ = 2500;
+      const radian = 2 * Math.atan(cameraHeight / 2 / cameraZ);
+      const fov = radian * (180 / Math.PI);
+      world.camera.near = near;
+      world.camera.far = far;
+      world.camera.aspect = aspect;
+      world.camera.fov = fov;
+      world.camera.updateProjectionMatrix();
+    }, 500);
+  });
+}
+
+function onPointerMove(event) {
+  mouse.x = (event.clientX / canvasRect.width) * 2 - 1;
+  mouse.y = -(event.clientY / canvasRect.height) * 2 + 1;
+}
+
+function raycast() {
+  raycaster.setFromCamera(mouse, world.camera);
+  const intersects = raycaster.intersectObjects(world.scene.children);
+  const intersect = intersects[0];
+
+  for (let i = 0; i < world.scene.children.length; i++) {
+    const _mesh = world.scene.children[i];
+
+    const uHover = _mesh.material.uniforms.uHover;
+    if (intersect && intersect.object === _mesh) {
+      _mesh.material.uniforms.uMouse.value = intersect.uv;
+      uHover.__endValue = 1;
+    } else {
+      uHover.__endValue = 0;
+    }
+    uHover.value = lerp(uHover.value, uHover.__endValue, 0.001);
+  }
+}
+
+function lerp(a, b, n) {
+  let current = (1 - n) * a + n * b;
+  if (Math.abs(b - current) < 0.001) current = b;
+  return current;
+}
+window.addEventListener( 'pointermove', onPointerMove );
+
+function scroll(o) {
+  const {
+    mesh,
+    $: { el },
+    canvasRect,
+  } = o;
+  const rect = el.getBoundingClientRect();
+  const { x, y } = getWorldPosition(rect, canvasRect);
+  // mesh.position.x = x;
+  mesh.position.y = y;
+}
+
+function initScroll() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const pageContainer = iNode.qs("#page-container");
+
+  const scrollBar = Scrollbar.init(pageContainer, { delegateTo: document });
+
+  ScrollTrigger.scrollerProxy(pageContainer, {
+    scrollTop(value) {
+      if (arguments.length) {
+        scrollBar.scrollTop = value; // setter
+      }
+      return scrollBar.scrollTop; // getter
+    },
+    // getBoundingClientRect() {
+    //   return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    // }
+  });
+
+  scrollBar.addListener(ScrollTrigger.update);
+
+  ScrollTrigger.defaults({
+    scroller: pageContainer,
+  });
+
+  const el = iNode.qs("[data-webgl]");
+
+  const meshX = os[0].mesh.position.x;
+  const meshY = os[0].mesh.position.y;
+  const animation = {
+    rotation: 0,
+    x: meshX,
+    y: meshY,
+  };
+  gsap.to(animation, {
+    rotation: Math.PI * 2,
+    x: meshX,
+    y: meshY,
+    scrollTrigger: {
+      trigger: el,
+      start: "top 50%",
+      // end: "top 30%",
+      scrub: true,
+      pin: true,
+    },
+    onUpdate() {
+      os[0].mesh.position.x = animation.x;
+      os[0].mesh.rotation.z = animation.rotation;
+    },
+  });
+}
+
+async function loadTex(url) {
+  const texLoader = new THREE.TextureLoader();
+  const texture = await texLoader.loadAsync(url);
+  texture.wrapS = THREE.ClampToEdgeWrapping;
+  texture.wrapT = THREE.MirroredRepeatWrapping;
+  return texture;
 }
--- src/blur_debug/fragment.glsl
+++ src/blur_debug/fragment.glsl
@@ -1,5 +1,8 @@
 precision mediump float;
 
+uniform vec2 uMouse;
+uniform float uHover;
+
 varying vec2 vUv;
 uniform sampler2D uTex1;
 uniform float uTick;
@@ -17,13 +20,17 @@ void main() {
   vec3 blurredColor = vec3(0.0);
 
   // ガウスぼかしの計算
-  for (int i = 0; i < kernelSize; i++) {
-    for (int j = 0; j < kernelSize; j++) {
-      vec2 offset = vec2(float(i - kernelSize/2), float(j - kernelSize/2)) * 0.003;  // ぼかしの強さを調整
+  for(int i = 0; i < kernelSize; i++) {
+    for(int j = 0; j < kernelSize; j++) {
+      vec2 offset = vec2(float(i - kernelSize / 2), float(j - kernelSize / 2)) * 0.003;  // ぼかしの強さを調整
       blurredColor += texture2D(uTex1, uv + offset).rgb * kernel[i] * kernel[j];
     }
   }
 
   gl_FragColor = vec4(blurredColor, 1.0);
   gl_FragColor = color;
+  gl_FragColor = vec4(1.0, 0.0, 0.0, 0.3);
+
+  vec2 mouse = step(uMouse, vUv);
+  gl_FragColor = vec4(mouse, uHover, 1.);
 }
