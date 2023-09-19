9eba531abbff10a9a549bcd05fa83059f6e809e5
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-19 17:44:39 +0900

    refactor:#01 create file of glsl

---
+++ src/03_02_obextend/scripts/glsl/normal/fragment.glsl
@@ -0,0 +1,27 @@
+
+varying vec2 vUv;
+                    uniform vec2 uMouse;
+                    uniform float uHover;
+                    uniform vec4 uResolution;
+                    uniform sampler2D tex1;
+                    uniform sampler2D tex2;
+            
+    
+                    vec2 coverUv(vec2 uv, vec4 resolution) {
+                      return (uv - .5) * resolution.zw + .5;
+                    }
+    
+                    void main() {
+    
+                      vec2 uv = coverUv(vUv, uResolution);
+    
+    
+                      vec4 t1 = texture2D(tex1, uv);
+                      vec4 t2 = texture2D(tex2, vUv);
+                      // vec2 mouse = step(uMouse, vUv);
+                      // gl_FragColor = vec4(mouse, uHover, 1.);
+                      gl_FragColor = mix(t1, t2, uHover);
+                    //   gl_FragColor = mix(t1, t2, step(0.5, vUv.x));
+                    //   gl_FragColor = t1;
+    
+                    }
