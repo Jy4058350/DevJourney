ffecfcfc6ec144dbb24fa1b3763b45cc627b28af
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-24 06:38:40 +0900

    chore:#06 dynamicimport used

--- src/03_08_debug_implementOrbit/scripts/bootstrap.js
+++ src/03_08_debug_implementOrbit/scripts/bootstrap.js
@@ -6,6 +6,8 @@ import { scroll } from "./component/scroll";
 import { mouse } from "./component/mouse";
 import { loader } from "./component/loader";
 import { gui } from "./helper/gui";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { AxesHelper } from "three";
 
 window.debug = debugmode(1) ? 1 : 0;
 
@@ -45,7 +47,26 @@ export async function init() {
 
   loader.loadingAnimation();
 
-  console.log(world.scene);
+  // const controls = new OrbitControls(world.camera, world.renderer.domElement);
+  // controls.enableDamping = true;
+  // const axis = new AxesHelper(100);
+  // world.scene.add(axis);
+
+  import("three/examples/jsm/controls/OrbitControls")
+    .then((module) => {
+      const { OrbitControls } = module;
+
+      const controls = new OrbitControls(
+        world.camera,
+        world.renderer.domElement
+      );
+      controls.enableDamping = true;
+      const axis = new AxesHelper(100);
+      world.scene.add(axis);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 
   gui.open();
 }
