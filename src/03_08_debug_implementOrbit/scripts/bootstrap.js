b8573cb2921b359c4c070ce39551f7bf125a86b4
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-24 06:42:50 +0900

    chore#07 implement basic function

--- src/03_08_debug_implementOrbit/scripts/bootstrap.js
+++ src/03_08_debug_implementOrbit/scripts/bootstrap.js
@@ -6,7 +6,6 @@ import { scroll } from "./component/scroll";
 import { mouse } from "./component/mouse";
 import { loader } from "./component/loader";
 import { gui } from "./helper/gui";
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { AxesHelper } from "three";
 
 window.debug = debugmode(1) ? 1 : 0;
@@ -47,26 +46,24 @@ export async function init() {
 
   loader.loadingAnimation();
 
-  // const controls = new OrbitControls(world.camera, world.renderer.domElement);
-  // controls.enableDamping = true;
-  // const axis = new AxesHelper(100);
-  // world.scene.add(axis);
-
-  import("three/examples/jsm/controls/OrbitControls")
-    .then((module) => {
-      const { OrbitControls } = module;
-
-      const controls = new OrbitControls(
-        world.camera,
-        world.renderer.domElement
-      );
-      controls.enableDamping = true;
-      const axis = new AxesHelper(100);
-      world.scene.add(axis);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-
   gui.open();
+
+  function orbitGo() {
+    import("three/examples/jsm/controls/OrbitControls")
+      .then((module) => {
+        const { OrbitControls } = module;
+
+        const controls = new OrbitControls(
+          world.camera,
+          world.renderer.domElement
+        );
+        controls.enableDamping = true;
+        const axis = new AxesHelper(100);
+        world.scene.add(axis);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+  orbitGo();
 }
