f9e1e42396f4fb625a3ec2a991d807b7a5542ece
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-25 15:01:52 +0900

    fix:#01 sticky action is not difference perform

--- src/03_09_effect1/scripts/glsl/sticky-plane/index.js
+++ src/03_09_effect1/scripts/glsl/sticky-plane/index.js
@@ -1,4 +1,6 @@
+import { PlaneGeometry, Float32BufferAttribute } from "three";
 import { CustomObject } from "../CustomObject";
+import { gsap } from "gsap";
 
 import vertexShader from "./vertex.glsl";
 import fragmentShader from "./fragment.glsl";
@@ -10,6 +12,73 @@ class ExtendObject extends CustomObject {
 
   fixFragment() {
     return fragmentShader;
+  }
+
+  setupGeometry() {
+    const wSeg = 30,
+      hSeg = 30;
+    const geometry = new PlaneGeometry(
+      this.rect.width,
+      this.rect.height,
+      wSeg,
+      hSeg
+    );
+
+    // 対角線上に詰められた遅延時間用の頂点データ
+    const delayVertices = getDiagonalVertices(hSeg, wSeg, getValue, 0);
+    printMat(delayVertices, wSeg + 1, "遅延時間行列");
+
+    // 0~1までの値をstep毎に返す
+    function getValue(previousValue, currentIndex) {
+      let step = 1 / (hSeg + 1) / (wSeg + 1);
+      return previousValue + step;
+    }
+
+    // 対角線上に頂点を詰めた配列を返す
+    function getDiagonalVertices(hSeg, wSeg, getValue, defaultValue) {
+      const hSeg1 = hSeg + 1,
+        wSeg1 = wSeg + 1;
+      let arry = [],
+        currentValue = defaultValue;
+      for (let i = 0; i < hSeg1 + wSeg1 - 1; i++) {
+        for (
+          let j = Math.min(hSeg1, i + 1) - 1;
+          j >= Math.max(0, i - wSeg1 + 1);
+          j--
+        ) {
+          let currentIndex = j * wSeg1 + i - j;
+          currentValue = getValue(currentValue, currentIndex);
+          arry[currentIndex] = currentValue;
+        }
+      }
+      return arry;
+    }
+
+    geometry.setAttribute(
+      "aDelay",
+      new Float32BufferAttribute(delayVertices, 1)
+    );
+
+    return geometry;
+  }
+
+  //   debug(toFolder) {
+  //     toFolder.add(this.uniforms.uEdge, "value", 0, 1, 0.1);
+  //   }
+  debug(toFolder) {
+    toFolder
+      .add(this.uniforms.uProgress, "value", 0, 1, 0.1)
+      .name("progess")
+      .listen();
+
+    const datData = { next: !!this.uniforms.uProgress.value };
+    toFolder.add(datData, "next").onChange(() => {
+      gsap.to(this.uniforms.uProgress, {
+        value: +datData.next,
+        duration: 2,
+        ease: "power2.inOut",
+      });
+    });
   }
 }
 
