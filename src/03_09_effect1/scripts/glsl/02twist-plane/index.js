b814c6056fe09fcd6bdf293a32867f277a418226
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-28 12:23:07 +0900

    chore:#01 add on doubleside

---
+++ src/03_09_effect1/scripts/glsl/02twist-plane/index.js
@@ -0,0 +1,78 @@
+import { CustomObject } from "../CustomObject";
+import gsap from "gsap";
+
+import vertexShader from "./vertex.glsl";
+import fragmentShader from "./fragment.glsl";
+
+class ExtendObject extends CustomObject {
+  setupGeometry() {
+    const wSeg = 30,
+      hSeg = 30;
+    const geometry = new THREE.PlaneGeometry(600, 300, wSeg, hSeg);
+
+    // 対角線上に詰められた遅延時間用の頂点データ
+    const delayVertices = getDiagonalVertices(hSeg, wSeg, getValue, 0);
+    //  printMat(delayVertices, wSeg + 1, '遅延時間行列');
+
+    // 0~1までの値をstep毎に返す
+    function getValue(previousValue, currentIndex) {
+      let step = 1 / (hSeg + 1) / (wSeg + 1);
+      return previousValue + step;
+    }
+
+    // 対角線上に頂点を詰めた配列を返す
+    function getDiagonalVertices(hSeg, wSeg, getValue, defaultValue) {
+      const hSeg1 = hSeg + 1,
+        wSeg1 = wSeg + 1;
+      let arry = [],
+        currentValue = defaultValue;
+      for (let i = 0; i < hSeg1 + wSeg1 - 1; i++) {
+        for (
+          let j = Math.min(hSeg1, i + 1) - 1;
+          j >= Math.max(0, i - wSeg1 + 1);
+          j--
+        ) {
+          let currentIndex = j * wSeg1 + i - j;
+          currentValue = getValue(currentValue, currentIndex);
+          arry[currentIndex] = currentValue;
+        }
+      }
+      return arry;
+    }
+
+    console.log(delayVertices);
+
+    geometry.setAttribute(
+      "aDelay",
+      new THREE.Float32BufferAttribute(delayVertices, 1)
+    );
+
+    return geometry;
+  }
+
+  fixVertex() {
+    return vertexShader;
+  }
+
+  fixFragment() {
+    return fragmentShader;
+  }
+
+  debug(toFolder) {
+    toFolder
+      .add(this.uniforms.uProgress, "value", 0, 1, 0.1)
+      .name("progess")
+      .listen();
+
+    const datData = { next: !!this.uniforms.uProgress.value };
+    toFolder.add(datData, "next").onChange(() => {
+      gsap.to(this.uniforms.uProgress, {
+        value: +datData.next,
+        duration: 2,
+        ease: "power2.inOut",
+      });
+    });
+  }
+}
+
+export default ExtendObject;
