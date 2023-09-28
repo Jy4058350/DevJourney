9da9b69d0aa573285ccd549747f1a8b449b77fd2
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-09-28 13:53:57 +0900

    first

---
+++ src/03_09_effect1/scripts/glsl/03noise-slide/index.js
@@ -0,0 +1,48 @@
+import { Vector2 } from "three";
+import vertexShader from "./vertex.glsl";
+import fragmentShader from "./fragment.glsl";
+
+import { CustomObject } from "../CustomObject";
+
+class ExtendObject extends CustomObject {
+  fixUniforms() {
+    const uniforms = super.fixUniforms();
+    uniforms.uNoseScale = { value: new Vector2(2, 2) };
+
+    return uniforms;
+  }
+
+  fixVertex() {
+    return vertexShader;
+  }
+
+  fixFragment() {
+    return fragmentShader;
+  }
+
+  debug(toFolder) {
+    toFolder
+      .add(this.uniforms.uNoiseScale.value, "value", 0, 1, 0.1)
+      .name("x")
+      .listen();
+    toFolder
+      .add(this.uniforms.uNoiseScale.value, "value", 0, 1, 0.1)
+      .name("y")
+      .listen();
+    toFolder
+      .add(this.uniforms.uProgress, "value", 0, 1, 0.1)
+      .name("value")
+      .listen();
+
+    const datData = { next: !!this.uniforms.uProgress.value };
+    toFolder.add(datData, "next").onChange(() => {
+      gsap.to(this.uniforms.uProgress, {
+        value: +datData.next,
+        duration: 2,
+        ease: "power2.inOut",
+      });
+    });
+  }
+}
+
+export default ExtendObject;
