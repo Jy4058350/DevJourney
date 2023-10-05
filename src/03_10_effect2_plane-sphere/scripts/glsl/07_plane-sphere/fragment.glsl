21ef6e5bcbdd283e080f5d8bb14658c15c7f4944
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-05 17:50:07 +0900

    chore:#02 until sphere darw implement

--- src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere/fragment.glsl
+++ src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere/fragment.glsl
@@ -1,23 +1,28 @@
 varying vec2 vUv;
+varying float vDelay;
 uniform vec2 uMouse;
 uniform vec4 uResolution;
+uniform float uTick;
+uniform float uSaturation;
+uniform float uBrightness;
+uniform float uColorTime;
+uniform float uColorDelay;
 uniform float uHover;
 uniform sampler2D tex1;
 uniform sampler2D tex2;
 uniform float uProgress;
+
+#pragma glslify: hsl12rgb = require(glsl-hsl2rgb);
 
 vec2 coverUv(vec2 uv, vec4 resolution) {
     return (uv - .5) * resolution.zw + .5;
 }
 
 void main() {
-          // vec2 mouse = step(uMouse, vUv);
-          // gl_FragColor = vec4(mouse, uHover, 1.);
+    vec4 tex = texture(tex1, gl_PointCoord);
 
-    vec2 uv = coverUv(vUv, uResolution);
+    float hue = sin(uTick * uColorTime - vDelay * uColorDelay) * .5 + .5;
+    vec3 rgb = hsl12rgb(vec3(hue, uSaturation, uBrightness));
+    gl_FragColor = vec4(rgb, 1.0);
 
-    vec4 t1 = texture2D(tex1, uv);
-    vec4 t2 = texture2D(tex2, uv);
-    vec4 color = mix(t1, t2, uProgress);
-    gl_FragColor = color;
 }
