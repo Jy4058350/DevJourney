5247fe7ae19314a076959bd7ea7fb59a90e559a2
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-05 15:24:32 +0900

    first

---
+++ src/03_09_effect1/scripts/glsl/gray-pic/index.js
@@ -0,0 +1,16 @@
+import vertexShader from "./vertex.glsl";
+import fragmentShader from "./fragment.glsl";
+
+import { CustomObject } from "../CustomObject";
+
+class ExtendObject extends CustomObject {
+  fixVertex() {
+    return vertexShader;
+  }
+
+  fixFragment() {
+    return fragmentShader;
+  }
+}
+
+export default ExtendObject;
