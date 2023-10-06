ea4cd44432e095fb9650768382345494f27adc38
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-06 12:46:32 +0900

    store it in a variable with using distance()methodo

--- src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere2/vertex.glsl
+++ src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere2/vertex.glsl
@@ -10,8 +10,13 @@ varying vec2 vUv;
 void main() {
     vUv = uv;
 
-    float try = distance(uv, vec2(0.5, 0.5));
-    vec3 pos = mix(position, sphere, uProgress);
+    float distancFromCenter = distance(uv, vec2(0.5, 0.5));
+
+    float progress = uProgress * 2.0 - distancFromCenter;
+
+    progress = clamp(progress, 0.0, 1.0);
+
+    vec3 pos = mix(sphere, position, progress);
 
     vec4 mvPosition = modelViewMatrix * vec4(pos, 1.0);
 
