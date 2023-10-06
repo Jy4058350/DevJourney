bba2bbb3d9a010b9279b86f9ecd6e80e2bf1a27d
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-06 12:16:35 +0900

    chore:#04 try to adjustment corner

--- src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere2/vertex.glsl
+++ src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere2/vertex.glsl
@@ -6,19 +6,14 @@ attribute vec3 sphere;
 uniform float uProgress;
 
 varying vec2 vUv;
-varying float vDelay;
 
 void main() {
     vUv = uv;
-    vDelay = aDelay;
 
-    float delay = clamp(uProgress * 3.0 - (1.0 - uv.x), 0.0, 1.0);
-    // vec3 pos = mix(position, sphere, delay);
+    float try = distance(uv, vec2(0.5, 0.5));
     vec3 pos = mix(position, sphere, uProgress);
 
     vec4 mvPosition = modelViewMatrix * vec4(pos, 1.0);
-
-    // gl_PointSize = 7.0 * (1000.0 / -mvPosition.z);
 
     gl_Position = projectionMatrix * mvPosition;
 }
