1b3d15ecd7c8f726a1878e0e95d43fd4ea4d390d
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-07 16:31:53 +0900

    fix:#05 scroll is not perform.

--- src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere3/index.js
+++ src/03_10_effect2_plane-sphere/scripts/glsl/07_plane-sphere3/index.js
@@ -100,6 +100,9 @@ class ExtendObject extends CustomObject {
   render(tick) {
     const renderer = super.render(tick);
 
+    this.mesh.position.x = this.uniforms.uMouse.value.x * 100;
+    this.mesh.position.y = this.uniforms.uMouse.value.y * 100;
+
     this.mesh.rotation.x =
       ((this.uniforms.uMouse.value.y - 0.5) * this.uniforms.uHover.value) / 1.5;
     this.mesh.rotation.y =
