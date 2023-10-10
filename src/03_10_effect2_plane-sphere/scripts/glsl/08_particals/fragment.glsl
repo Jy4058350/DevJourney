0069f30b4a0b106e5fa9e5a017265efed30dce76
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-11 06:23:07 +0900

    build shader-functionHelper curl,parabola

--- src/03_10_effect2_plane-sphere/scripts/glsl/08_particals/fragment.glsl
+++ src/03_10_effect2_plane-sphere/scripts/glsl/08_particals/fragment.glsl
@@ -10,7 +10,7 @@ uniform float uHover;
 uniform sampler2D tex1;
 uniform sampler2D tex2;
 varying float vScalar;
-varying vec3 vSphereNormal;
+// varying vec3 vSphereNormal;
 
 #pragma glslify: grayscale = require(../shader-helper/grayscale);
 #pragma glslify: coverUv = require(../shader-helper/coverUv);
@@ -30,19 +30,20 @@ void main() {
     vec3 ray = vec3(cos(uTick * 0.01) * 0.3, sin(uTick * 0.01) * 0.3, 0.5);
     // vec3 ray = vec3(cos(uTick * 0.01), 0.0, sin(uTick * 0.01));
     // float fresnel = dot(ray, vSphereNormal);
-    float fresnel = 1.0 - dot(ray, vSphereNormal);
+    // float fresnel = 1.0 - dot(ray, vSphereNormal);
     // vec4 sphereColor = vec4(vec3(fresnel), 1.0 - fresnel);
-    vec4 sphereColor = vec4(vec3(fresnel), 0.5);
-    vec4 t = vec4(1.0, 0.0, 0.0, 1.0);
+    // vec4 sphereColor = vec4(vec3(fresnel), 0.5);
+    // vec4 t = vec4(1.0, 0.0, 0.0, 1.0);
     // vec4 b = mix(sphereColor, t, 1.0 - vScalar);
     // vec4 b = mix(sphereColor, t, vScalar);
-    vec4 b = mix(sphereColor, t, 0.5);
-    vec4 c = grayscale(b);
-    vec4 d = mix(c, b, uHover);
+    // vec4 b = mix(sphereColor, t, 0.5);
+    // vec4 c = grayscale(b);
+    // vec4 d = mix(c, b, uHover);
 
     //mix colors
     // vec4 color = mix(sphereColor, texColor, vScalar);
-    vec4 color = mix(d, texColor, vScalar);
-    gl_FragColor = color;
+    // vec4 color = mix(d, texColor, vScalar);
+    // gl_FragColor = color;
+    gl_FragColor = tex;
 
 }
--- src/03_10_effect2_plane-sphere/scripts/glsl/08_particals/vertex.glsl
+++ src/03_10_effect2_plane-sphere/scripts/glsl/08_particals/vertex.glsl
@@ -2,31 +2,31 @@ precision mediump float;
 #pragma glslify: spherenoise = require(glsl-noise/simplex/3d);
 
 attribute float aDelay;
-attribute vec3 sphereNormal;
+// attribute vec3 sphereNormal;
 attribute vec3 sphere;
 
 uniform float uProgress;
 
-uniform float uSphereRadius;
+// uniform float uSphereRadius;
 uniform float uTick;
-uniform float strength;
-uniform float frequency;
-uniform float noiseScale;
+// uniform float strength;
+// uniform float frequency;
+// uniform float noiseScale;
 varying vec2 vUv;
 varying float vScalar;
-varying vec3 vSphereNormal;
+// varying vec3 vSphereNormal;
 
 void main() {
 
     //add noise
-    vec3 sphere = sphere;
-    float noise = spherenoise(vec3((sphereNormal.x) * noiseScale - uTick * frequency, (sphereNormal.y)* noiseScale - uTick * frequency , (sphereNormal.z)* noiseScale - uTick * frequency ));
-    sphere += sphere * noise * strength;
+    // vec3 sphere = sphere;
+    // float noise = spherenoise(vec3((sphereNormal.x) * noiseScale - uTick * frequency, (sphereNormal.y)* noiseScale - uTick * frequency , (sphereNormal.z)* noiseScale - uTick * frequency ));
+    // sphere += sphere * noise * strength;
 
     //calculate scalar
     vec3 p = position;
     vUv = uv;
-    vSphereNormal = sphereNormal + sphereNormal * noise * 0.3;
+    // vSphereNormal = sphereNormal + sphereNormal * noise * 0.3;
 
     float distanceFromCenter = distance(uv, vec2(0.5, 0.5));
 
@@ -36,9 +36,9 @@ void main() {
 
     vScalar = scalar;
 
-    vec3 pos = mix(sphere * uSphereRadius, position, scalar);
+    // vec3 pos = mix(sphere * uSphereRadius, position, scalar);
 
-    vec4 mvPosition = modelViewMatrix * vec4(pos, 1.0);
+    vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
 
     gl_Position = projectionMatrix * mvPosition;
 }
