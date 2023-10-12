cba77b243cdfa1bc04d480285c0fd844e17c2adc
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-12 20:22:14 +0900

    feat:#11 zoomTime is control

--- src/00_dev/scripts/glsl/shader-helper/zoomUv.glsl
+++ src/00_dev/scripts/glsl/shader-helper/zoomUv.glsl
@@ -2,7 +2,7 @@ vec2 zoomUv(vec2 uv, vec4 resolution, float uProgress) {
     // return (uv - .5) * resolution.zw * 0.8 + .5;
 
     // float progress = (1.0 - uProgress);
-    float zoom = clamp(uProgress, 0.0, 0.2);
+    float zoom = clamp(uProgress * 0.1, 0.0, 0.2);
     return (uv - .5) * resolution.zw * (1.0 - zoom) + .5;
 }
 
