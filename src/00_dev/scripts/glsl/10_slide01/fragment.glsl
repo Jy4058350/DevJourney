e48ef633506032eeef7bc02e347b0ed0b90b511f
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-12 20:08:23 +0900

    test:#09 scaling test is expected peform

--- src/00_dev/scripts/glsl/10_slide01/fragment.glsl
+++ src/00_dev/scripts/glsl/10_slide01/fragment.glsl
@@ -4,20 +4,20 @@ uniform sampler2D tex1;
 uniform sampler2D tex2;
 uniform float uProgress;
 
-// vec2 coverUv(vec2 uv, vec4 resolution) {
-//     return (uv - .5) * resolution.zw + .5;
-// }
-
 #pragma glslify: coverUv = require(../shader-helper/coverUv);
+#pragma glslify: zoomUv = require(../shader-helper/zoomUv);
 
 void main() {
 
     vec2 uv = coverUv(vUv, uResolution);
+    vec2 zoomedUv = zoomUv(uv, uResolution);
 
     vec4 t1 = texture2D(tex1, uv);
     vec4 t2 = texture2D(tex2, uv);
+    vec4 t1a = texture2D(tex1, zoomedUv);
+    vec4 t2a = texture2D(tex2, zoomedUv);
 
-    t1.a = uProgress;
+    // t1.a = uProgress;
     t2.a = 1.0 - uProgress;
 
     //variable definition
@@ -25,4 +25,7 @@ void main() {
 
     vec4 color = mix(t1, t2, smoothstep(uProgress, uProgress + variable, uv.y));
     gl_FragColor = color;
+
+    vec4 zcolor = mix(t1, t1a, uProgress);
+    gl_FragColor = zcolor;
 }
