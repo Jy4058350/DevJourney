18a4f71ea5d1601b4d1dbcfe8590d0a909c2f242
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-13 17:39:16 +0900

    test:#08 _box become effective

--- src/00_dev/scripts/helper/gsap.js
+++ src/00_dev/scripts/helper/gsap.js
@@ -1,44 +1,42 @@
 import gsap from "gsap";
 
+const _box = new Map();
 function startGsapAnimation(uniforms) {
-  const box = new Map();
   const uni = uniforms;
   for (let key in uni) {
     if (!key.startsWith("uProgress")) continue;
     if (key.startsWith("uProgress")) {
-      box.set(key, uni[key]);
-      console.log(box);
+      _box.set(key, uni[key]);
+      console.log(_box);
     }
   }
-
-  
-  //   const tl = new gsap.timeline({
-  //     onComplete: () => {
-  //       //   console.log(uniforms);
-  //       uniforms.uProgress.value = 0;
-  //       uniforms.uIndex.value = 0;
-  //       startGsapAnimation(uniforms);
-  //     },
-  //   });
-  //   tl.to(uniforms.uProgress, {
-  //     value: 1.0,
-  //     duration: 1.0,
-  //     ease: "ease",
-  //     onComplete: () => {
-  //       uniforms.uIndex.value = 0;
-  //       tl.to(uniforms.uProgress1, {
-  //         value: 1.0,
-  //         duration: 1.0,
-  //         onComplete: () => {
-  //           uniforms.uIndex.value = 1;
-  //         },
-  //       });
-  //     },
-  //   });
 }
 
 function gsapActive() {
-    console.log("gsapActive");
-  }
+    console.log(_box);
+  const tl = new gsap.timeline({
+    onComplete: () => {
+      console.log(_box);
+      uniforms.uProgress.value = 0;
+      uniforms.uIndex.value = 0;
+      startGsapAnimation(uniforms);
+    },
+  });
+  // tl.to(uniforms.uProgress, {
+  //   value: 1.0,
+  //   duration: 1.0,
+  //   ease: "ease",
+  //   onComplete: () => {
+  //     uniforms.uIndex.value = 0;
+  //     tl.to(uniforms.uProgress1, {
+  //       value: 1.0,
+  //       duration: 1.0,
+  //       onComplete: () => {
+  //         uniforms.uIndex.value = 1;
+  //       },
+  //     });
+  //   },
+  // });
+}
 
 export { startGsapAnimation, gsapActive };
