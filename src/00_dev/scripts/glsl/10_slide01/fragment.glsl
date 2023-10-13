6a5fd48445f6f1a30c89591d3c8eb9aaec622722
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-13 09:46:59 +0900

    chore:#12 uProgress comb is not perform

--- src/00_dev/scripts/glsl/10_slide01/fragment.glsl
+++ src/00_dev/scripts/glsl/10_slide01/fragment.glsl
@@ -3,6 +3,7 @@ uniform vec4 uResolution;
 uniform sampler2D tex1;
 uniform sampler2D tex2;
 uniform float uProgress;
+uniform float uTick;
 
 #pragma glslify: coverUv = require(../shader-helper/coverUv);
 #pragma glslify: zoomUv = require(../shader-helper/zoomUv);
@@ -17,14 +18,18 @@ void main() {
     vec4 t1a = texture2D(tex1, zoomedUv);
     vec4 t2a = texture2D(tex2, zoomedUv);
 
-    // t1.a = uProgress;
-    t2.a = 1.0 - uProgress;
+    t1.a = 1.0 - uProgress;
+    t2.a = uProgress;
 
     //variable definition
     float variable = clamp(uProgress, 0.0, 0.5);
 
-    vec4 color = mix(t1, t2, smoothstep(uProgress, uProgress + variable, uv.y));
+    vec4 color = mix(t2, t1, smoothstep(uProgress, uProgress + variable, uv.y));
     gl_FragColor = color;
-    gl_FragColor = t1a;
+    // gl_FragColor = t1a;
 
+    if(uProgress > 0.9) {
+
+        gl_FragColor = t2a;
+    }
 }
--- src/00_dev/scripts/glsl/10_slide01/index.js
+++ src/00_dev/scripts/glsl/10_slide01/index.js
@@ -24,7 +24,7 @@ class ExtendObject extends CustomObject {
 toFolder.add(datData, "next").onChange(() => {
   gsap.to(this.uniforms.uProgress, {
     value: datData.next ? 1 : 0,
-    duration: 1.0,
+    duration: 2.0,
     ease: "none",
   });
 })
