98ae6393ae5e38302f92e5a2ba058d05b94079f8
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-20 10:37:55 +0900

    feat:#09 Successfully displayed by index control

--- src/00_dev/scripts/glsl/15_sliderNumber/fragment.glsl
+++ src/00_dev/scripts/glsl/15_sliderNumber/fragment.glsl
@@ -36,161 +36,160 @@ uniform int textureIndex;
 
 void main() {
 
+    // float radius = radius;
+    // float radius1 = radius1;
 
+    // float distance = distanceFromCenter(vUv, uResolution);
+    // //Common variables
+    // vec2 u = coverUv(vUv, uResolution);
+    // vec2 zoomedUv2 = zoomUv2(vUv, uResolution, uProgress, uTick, uRaito);
+    // vec2 zoomedUv3 = zoomUv3(vUv, uResolution, uProgress, uTick, uRaito);
+    // // vec2 center = vec2(0.5, 0.5);
 
-    float radius = radius;
-    float radius1 = radius1;
+    // // 4 pan transitions
+    // vec2 uv = panUv(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
+    // vec2 uv1 = panUv1(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
+    // vec2 uv2 = panUv2(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
+    // vec2 uv3 = panUv3(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
 
-    float distance = distanceFromCenter(vUv, uResolution);
-    //Common variables
-    vec2 u = coverUv(vUv, uResolution);
-    vec2 zoomedUv2 = zoomUv2(vUv, uResolution, uProgress, uTick, uRaito);
-    vec2 zoomedUv3 = zoomUv3(vUv, uResolution, uProgress, uTick, uRaito);
-    // vec2 center = vec2(0.5, 0.5);
+    // //try other slide transitions here 7textures
+    // int currentTexture = int(uProgress * 7.0);
+    // float texBlend = fract(uProgress * 7.0);
 
-    // 4 pan transitions
-    vec2 uv = panUv(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
-    vec2 uv1 = panUv1(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
-    vec2 uv2 = panUv2(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
-    vec2 uv3 = panUv3(vUv, uResolution, uProgress, uTick, xOffset, yOffset, uRaito);
+    // float angle = calculateAngle(uv, uProgress);
+    // if(uIndex == 0.0) {
+    // // Blend between the current and next textures
+    //     vec4 color; //lower left
 
-    //try other slide transitions here 7textures
-    int currentTexture = int(uProgress * 7.0);
-    float texBlend = fract(uProgress * 7.0);
+    //     if(distance < radius) {
+    //         color = texture2D(tex1, zoomedUv2);
+    //     } else {
+    //         if(distance < radius1) {
+    //         // if(angle >= 0.0 && angle < calculateAngle(uv, uProgress)) {
+    //             if(angle >= 0.0 && angle < uProgress * 270.0) {
+    //                 color = texture2D(tex1, uv);
+    //             } else {
+    //             // color = texture2D(tex4, uv);
+    //                 color = mix(texture2D(tex1, uv), texture2D(tex2, uv), texBlend);
+    //             }
 
-    float angle = calculateAngle(uv, uProgress);
+    //         } else if(currentTexture == 0) {
+    //             color = mix(texture2D(tex1, uv), texture2D(tex2, uv), texBlend);
+    //         } else if(currentTexture == 1) {
+    //             color = mix(texture2D(tex2, uv), texture2D(tex3, uv), texBlend);
+    //         } else if(currentTexture == 2) {
+    //             color = mix(texture2D(tex3, uv), texture2D(tex4, uv), texBlend);
+    //         } else if(currentTexture == 3) {
+    //             color = mix(texture2D(tex4, uv), texture2D(tex5, uv), texBlend);
+    //         } else if(currentTexture == 4) {
+    //             color = mix(texture2D(tex5, uv), texture2D(tex6, uv), texBlend);
+    //         } else if(currentTexture == 5) {
+    //             color = mix(texture2D(tex6, uv), texture2D(tex7, uv), texBlend);
+    //         } else if(currentTexture == 6) {
+    //             color = mix(texture2D(tex1, uv), texture2D(tex8, uv), texBlend);
+    //         } else {
+    //             color = texture2D(tex1, uv);
+
+    //         }
+    //     }
+    //     vec4 color1;
+    //     if(distance < radius) {
+    //         color1 = texture2D(tex1, zoomedUv2);
+    //     } else {
+    //         if(distance < radius1) {
+    //             if(angle >= 0.0 && angle < uProgress * 180.0) {
+    //                 color1 = texture2D(tex1, uv1);
+    //             } else {
+    //             // color1 = texture2D(tex4, uv1);
+    //                 color1 = mix(texture2D(tex1, uv1), texture2D(tex2, uv1), texBlend);
+    //             }
+    //         } else if(currentTexture == 0) {
+    //             color1 = mix(texture2D(tex1, uv1), texture2D(tex2, uv1), texBlend);
+    //         } else if(currentTexture == 1) {
+    //             color1 = mix(texture2D(tex2, uv1), texture2D(tex3, uv1), texBlend);
+    //         } else if(currentTexture == 2) {
+    //             color1 = mix(texture2D(tex3, uv1), texture2D(tex4, uv1), texBlend);
+    //         } else if(currentTexture == 3) {
+    //             color1 = mix(texture2D(tex4, uv1), texture2D(tex5, uv1), texBlend);
+    //         } else if(currentTexture == 4) {
+    //             color1 = mix(texture2D(tex5, uv1), texture2D(tex6, uv1), texBlend);
+    //         } else if(currentTexture == 5) {
+    //             color1 = mix(texture2D(tex6, uv1), texture2D(tex7, uv1), texBlend);
+    //         } else if(currentTexture == 6) {
+    //             color1 = mix(texture2D(tex1, uv1), texture2D(tex8, uv1), texBlend);
+    //         } else {
+    //             color1 = texture2D(tex1, uv1);
+    //         }
+    //     }
+    //     vec4 color2;
+    //     if(distance < radius) {
+    //         color2 = texture2D(tex1, zoomedUv2);
+    //     } else {
+    //         if(distance < radius1) {
+    //             if(angle >= 0.0 && angle < uProgress * 360.0) {
+    //                 color2 = texture2D(tex1, uv2);
+    //             } else {
+    //             // color2 = texture2D(tex4, uv2);
+    //                 color2 = mix(texture2D(tex1, uv2), texture2D(tex2, uv2), texBlend);
+    //             }
+    //         } else if(currentTexture == 0) {
+    //             color2 = mix(texture2D(tex1, uv2), texture2D(tex2, uv2), texBlend);
+    //         } else if(currentTexture == 1) {
+    //             color2 = mix(texture2D(tex2, uv2), texture2D(tex3, uv2), texBlend);
+    //         } else if(currentTexture == 2) {
+    //             color2 = mix(texture2D(tex3, uv2), texture2D(tex4, uv2), texBlend);
+    //         } else if(currentTexture == 3) {
+    //             color2 = mix(texture2D(tex4, uv2), texture2D(tex5, uv2), texBlend);
+    //         } else if(currentTexture == 4) {
+    //             color2 = mix(texture2D(tex5, uv2), texture2D(tex6, uv2), texBlend);
+    //         } else if(currentTexture == 5) {
+    //             color2 = mix(texture2D(tex6, uv2), texture2D(tex7, uv2), texBlend);
+    //         } else if(currentTexture == 6) {
+    //             color2 = mix(texture2D(tex1, uv2), texture2D(tex8, uv2), texBlend);
+    //         } else {
+    //             color2 = texture2D(tex1, uv2);
+    //         }
+    //     }
+    //     vec4 color3;
+    //     if(distance < radius) {
+    //         color3 = texture2D(tex1, zoomedUv2);
+    //     } else {
+    //         if(distance < radius1) {
+    //             if(angle >= 0.0 && angle < uProgress * 90.0) {
+    //                 color3 = texture2D(tex1, uv3);
+    //             } else {
+    //             // color3 = texture2D(tex1, uv3);
+    //                 color3 = mix(texture2D(tex1, uv3), texture2D(tex2, uv3), texBlend);
+    //             }
+    //         } else if(currentTexture == 0) {
+    //             color3 = mix(texture2D(tex1, uv3), texture2D(tex2, uv3), texBlend);
+    //         } else if(currentTexture == 1) {
+    //             color3 = mix(texture2D(tex2, uv3), texture2D(tex3, uv3), texBlend);
+    //         } else if(currentTexture == 2) {
+    //             color3 = mix(texture2D(tex3, uv3), texture2D(tex4, uv3), texBlend);
+    //         } else if(currentTexture == 3) {
+    //             color3 = mix(texture2D(tex4, uv3), texture2D(tex5, uv3), texBlend);
+    //         } else if(currentTexture == 4) {
+    //             color3 = mix(texture2D(tex5, uv3), texture2D(tex6, uv3), texBlend);
+    //         } else if(currentTexture == 5) {
+    //             color3 = mix(texture2D(tex6, uv3), texture2D(tex7, uv3), texBlend);
+    //         } else if(currentTexture == 6) {
+    //             color3 = mix(texture2D(tex1, uv3), texture2D(tex8, uv3), texBlend);
+    //         } else {
+    //             color3 = texture2D(tex1, uv3);
+    //         }
+    //     }
+    //     vec4 c = mix(color, color1, step(0.5, vUv.x));
+    //     vec4 d = mix(color2, color3, step(0.5, vUv.x));
+    //     vec4 e = mix(c, d, step(0.5, vUv.y));
+    //     gl_FragColor = e;
+    // }
+    // if(uIndex == 1.0) {
+    //     gl_FragColor = texture2D(tex1, zoomedUv3);
+    // }
+
     if(uIndex == 0.0) {
-    // Blend between the current and next textures
-        vec4 color; //lower left
+        gl_FragColor = texture2D(textures[5], vUv);
 
-        if(distance < radius) {
-            color = texture2D(tex1, zoomedUv2);
-        } else {
-            if(distance < radius1) {
-            // if(angle >= 0.0 && angle < calculateAngle(uv, uProgress)) {
-                if(angle >= 0.0 && angle < uProgress * 270.0) {
-                    color = texture2D(tex1, uv);
-                } else {
-                // color = texture2D(tex4, uv);
-                    color = mix(texture2D(tex1, uv), texture2D(tex2, uv), texBlend);
-                }
-
-            } else if(currentTexture == 0) {
-                color = mix(texture2D(tex1, uv), texture2D(tex2, uv), texBlend);
-            } else if(currentTexture == 1) {
-                color = mix(texture2D(tex2, uv), texture2D(tex3, uv), texBlend);
-            } else if(currentTexture == 2) {
-                color = mix(texture2D(tex3, uv), texture2D(tex4, uv), texBlend);
-            } else if(currentTexture == 3) {
-                color = mix(texture2D(tex4, uv), texture2D(tex5, uv), texBlend);
-            } else if(currentTexture == 4) {
-                color = mix(texture2D(tex5, uv), texture2D(tex6, uv), texBlend);
-            } else if(currentTexture == 5) {
-                color = mix(texture2D(tex6, uv), texture2D(tex7, uv), texBlend);
-            } else if(currentTexture == 6) {
-                color = mix(texture2D(tex1, uv), texture2D(tex8, uv), texBlend);
-            } else {
-                color = texture2D(tex1, uv);
-
-            }
-        }
-        vec4 color1;
-        if(distance < radius) {
-            color1 = texture2D(tex1, zoomedUv2);
-        } else {
-            if(distance < radius1) {
-                if(angle >= 0.0 && angle < uProgress * 180.0) {
-                    color1 = texture2D(tex1, uv1);
-                } else {
-                // color1 = texture2D(tex4, uv1);
-                    color1 = mix(texture2D(tex1, uv1), texture2D(tex2, uv1), texBlend);
-                }
-            } else if(currentTexture == 0) {
-                color1 = mix(texture2D(tex1, uv1), texture2D(tex2, uv1), texBlend);
-            } else if(currentTexture == 1) {
-                color1 = mix(texture2D(tex2, uv1), texture2D(tex3, uv1), texBlend);
-            } else if(currentTexture == 2) {
-                color1 = mix(texture2D(tex3, uv1), texture2D(tex4, uv1), texBlend);
-            } else if(currentTexture == 3) {
-                color1 = mix(texture2D(tex4, uv1), texture2D(tex5, uv1), texBlend);
-            } else if(currentTexture == 4) {
-                color1 = mix(texture2D(tex5, uv1), texture2D(tex6, uv1), texBlend);
-            } else if(currentTexture == 5) {
-                color1 = mix(texture2D(tex6, uv1), texture2D(tex7, uv1), texBlend);
-            } else if(currentTexture == 6) {
-                color1 = mix(texture2D(tex1, uv1), texture2D(tex8, uv1), texBlend);
-            } else {
-                color1 = texture2D(tex1, uv1);
-            }
-        }
-        vec4 color2;
-        if(distance < radius) {
-            color2 = texture2D(tex1, zoomedUv2);
-        } else {
-            if(distance < radius1) {
-                if(angle >= 0.0 && angle < uProgress * 360.0) {
-                    color2 = texture2D(tex1, uv2);
-                } else {
-                // color2 = texture2D(tex4, uv2);
-                    color2 = mix(texture2D(tex1, uv2), texture2D(tex2, uv2), texBlend);
-                }
-            } else if(currentTexture == 0) {
-                color2 = mix(texture2D(tex1, uv2), texture2D(tex2, uv2), texBlend);
-            } else if(currentTexture == 1) {
-                color2 = mix(texture2D(tex2, uv2), texture2D(tex3, uv2), texBlend);
-            } else if(currentTexture == 2) {
-                color2 = mix(texture2D(tex3, uv2), texture2D(tex4, uv2), texBlend);
-            } else if(currentTexture == 3) {
-                color2 = mix(texture2D(tex4, uv2), texture2D(tex5, uv2), texBlend);
-            } else if(currentTexture == 4) {
-                color2 = mix(texture2D(tex5, uv2), texture2D(tex6, uv2), texBlend);
-            } else if(currentTexture == 5) {
-                color2 = mix(texture2D(tex6, uv2), texture2D(tex7, uv2), texBlend);
-            } else if(currentTexture == 6) {
-                color2 = mix(texture2D(tex1, uv2), texture2D(tex8, uv2), texBlend);
-            } else {
-                color2 = texture2D(tex1, uv2);
-            }
-        }
-        vec4 color3;
-        if(distance < radius) {
-            color3 = texture2D(tex1, zoomedUv2);
-        } else {
-            if(distance < radius1) {
-                if(angle >= 0.0 && angle < uProgress * 90.0) {
-                    color3 = texture2D(tex1, uv3);
-                } else {
-                // color3 = texture2D(tex1, uv3);
-                    color3 = mix(texture2D(tex1, uv3), texture2D(tex2, uv3), texBlend);
-                }
-            } else if(currentTexture == 0) {
-                color3 = mix(texture2D(tex1, uv3), texture2D(tex2, uv3), texBlend);
-            } else if(currentTexture == 1) {
-                color3 = mix(texture2D(tex2, uv3), texture2D(tex3, uv3), texBlend);
-            } else if(currentTexture == 2) {
-                color3 = mix(texture2D(tex3, uv3), texture2D(tex4, uv3), texBlend);
-            } else if(currentTexture == 3) {
-                color3 = mix(texture2D(tex4, uv3), texture2D(tex5, uv3), texBlend);
-            } else if(currentTexture == 4) {
-                color3 = mix(texture2D(tex5, uv3), texture2D(tex6, uv3), texBlend);
-            } else if(currentTexture == 5) {
-                color3 = mix(texture2D(tex6, uv3), texture2D(tex7, uv3), texBlend);
-            } else if(currentTexture == 6) {
-                color3 = mix(texture2D(tex1, uv3), texture2D(tex8, uv3), texBlend);
-            } else {
-                color3 = texture2D(tex1, uv3);
-            }
-        }
-        vec4 c = mix(color, color1, step(0.5, vUv.x));
-        vec4 d = mix(color2, color3, step(0.5, vUv.x));
-        vec4 e = mix(c, d, step(0.5, vUv.y));
-        gl_FragColor = e;
     }
-    if(uIndex == 1.0) {
-        gl_FragColor = texture2D(tex1, zoomedUv3);
-    }
-
-    vec4 z = textures[0];
-
-    gl_FragColor = z;
 }
