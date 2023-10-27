11a99f1db1df21be0d7b53675fbd59f794ed9995
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-28 07:23:34 +0900

    chore:#03 Vertex implemention.Active slides are caluculated by mod.Define depth to give perspective

--- src/001_dev_style/scripts/glsl/textSlide/vertex.glsl
+++ src/001_dev_style/scripts/glsl/textSlide/vertex.glsl
@@ -7,5 +7,23 @@ uniform float uActiveIndex;
 
 void main() {
     vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+
+    vec3 pos = position;
+
+    float activeSlideIndex = mod(uActiveIndex, uSlideTotal);
+    float distance = abs(activeSlideIndex - uSlideIndex);
+    float depth = uSlideTotal / 2.;
+
+    float distanceProgress = abs(distance - depth) / depth;
+    vDistanceProgress = distanceProgress;  
+
+    float radius = uRadius * distanceProgress;
+
+    pos.y = pos.y * radius;
+
+    pos.x = pos.x * radius;
+    
+
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
 }
