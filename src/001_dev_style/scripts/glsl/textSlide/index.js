0817d440896c9d6f4fbd439d68fd1dde15ade391
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-28 06:55:28 +0900

    first:I am going to try to implement it to bring perspective from now on

--- src/001_dev_style/scripts/glsl/textSlide/index.js
+++ src/001_dev_style/scripts/glsl/textSlide/index.js
@@ -10,13 +10,12 @@ import { pointTo, lerp } from "../../helper/utils";
 class ExtendObject extends CustomObject {
   before() {
     this.radius = this.rect.width;
+    this.rotateAxis = new Vector3(0, 1, 0);
     this.differenceRadius = 0;
     this.activeIndex = 0;
-    this.rotateAxis = new Vector3(0, 1, 0);
   }
 
   fixGeometry() {
-    // return new PlaneGeometry(this.rect.width, this.rect.height, 1, 1);
     const geo = super.fixGeometry();
     geo.scale(0.5, 0.5, 0.5);
     return geo;
@@ -27,7 +26,7 @@ class ExtendObject extends CustomObject {
       this.radius,
       this.radius,
       this.rect.height / 2,
-      80,
+      100,
       1,
       true
     );
@@ -42,7 +41,8 @@ class ExtendObject extends CustomObject {
     // cylinder.position.z = -this.radius;
 
     const { position, normal } = cylinderGeo.attributes;
-    const oneLoop = cylinderGeo.attributes.position.count;
+    // const oneLoop = cylinderGeo.attributes.position.count;
+    const oneLoop = cylinderGeo.attributes.position.count / 2;
     const step = Math.floor(oneLoop / this.texes.size);
     console.log(step);
     let index = 0;
@@ -91,12 +91,8 @@ class ExtendObject extends CustomObject {
 
   goToNext(index) {
     this.differenceRadius -=
-      // ((index - this.activeIndex) * 2 * Math.PI) / this.texes.size;
       ((index - this.activeIndex) * 2 * Math.PI) / this.slides.length;
-    // this.differenceRadius +=
-    //   ((index - this.activeIndex) / this.slides.length) * 2 * Math.PI;
     this.activeIndex = index;
-    console.log(this.differenceRadius);
   }
 
   render(tick) {
@@ -104,20 +100,16 @@ class ExtendObject extends CustomObject {
     if (this.differenceRadius === 0) return;
 
     const rad = lerp(this.differenceRadius, 0, 0.95);
-    // this.mesh.rotateOnWorldAxis(this.rotateAxis, this.differenceRadius);
     this.mesh.rotateOnWorldAxis(this.rotateAxis, rad);
-    // this.differenceRadius = 0;
     this.differenceRadius -= rad;
   }
 
   debug(toFolder) {
-    // toFolder.add(this.uniforms.uEdge, "value", 0, 1, 0.1);
     toFolder
       .add(this.uniforms.uProgress, "value", 0, 1, 0.1)
       .name("progress")
       .listen();
 
-    // const datObj = { next: !!this.uniforms.uProgress.value };
     const idx = { value: 0 };
     toFolder
       .add(idx, "value", 0, 12, 1)
