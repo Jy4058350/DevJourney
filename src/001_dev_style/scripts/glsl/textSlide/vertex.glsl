188348c86e881ab72c6380a7318d9c674c636c7c
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-28 07:06:48 +0900

    chore:#02 uniforms were registered as a vertex float variable

--- src/001_dev_style/scripts/glsl/textSlide/vertex.glsl
+++ src/001_dev_style/scripts/glsl/textSlide/vertex.glsl
@@ -1,5 +1,10 @@
 varying vec2 vUv;
 
+uniform float uRadius;
+uniform float uSlideIndex;
+uniform float uSlideTotal;
+uniform float uActiveIndex;
+
 void main() {
     vUv = uv;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
