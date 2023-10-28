e1a6faf471793e14d7f0175cba6eeec88e544644
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-10-28 13:27:36 +0900

    Again, I assigned the variable I created for perspective to the third number of animals in the mix function

--- src/001_dev_style/scripts/glsl/textSlide/fragment.glsl
+++ src/001_dev_style/scripts/glsl/textSlide/fragment.glsl
@@ -1,8 +1,8 @@
 varying vec2 vUv;
 uniform vec4 uResolution;
 uniform sampler2D tex1;
-uniform float uProgress;
 
+varying float vDistanceProgress;
 varying float vScaleProgress;
 
 #pragma glslify: coverUv = require(../shader-helper/coverUv);
@@ -18,11 +18,8 @@ void main() {
     uv = uv * scale;
     uv = uv + 0.5;
 
-
-    vec4 t2 = vec4(1.0, 0.0, 0.0, 1.0);
-
     gl_FragColor = t1;
 
-    gl_FragColor = mix(t1, t2, uProgress);
+    gl_FragColor.a = mix(0.0, t1.a, vDistanceProgress);
 
 }
