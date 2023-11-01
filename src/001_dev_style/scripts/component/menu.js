46cd3c94945d99656dac145176a0af5bd84b5a73
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-11-01 14:51:36 +0900

    first

--- src/001_dev_style/scripts/component/menu.js
+++ src/001_dev_style/scripts/component/menu.js
@@ -23,22 +23,32 @@ function _handlePointerDownAndMouseEnter() {
   $.btn.addEventListener("mouseenter", _handleMouseEnter);
 }
 
-function _handlePointerDown() {}
+function _handlePointerDown() {
+  const tl = gsap.timeline();
+  tl.set($.wraps, {
+    rotate: 0,
+  }).to($.bars, {
+    height: "5px",
+    duration: 0.2,
+    backgroundColor: "black",
+    borderRadius: 5,
+  });
+}
 
 function _handleMouseEnter() {
   const tl = gsap.timeline({
     defaults: { duration: 0.2, stagger: 0.1, ease: "power1.inOut" },
   });
   tl.set($.bars, {
-    // transformOrigin: "right center",
-    transformOrigin: "right",
+    transformOrigin: "right center",
+    // transformOrigin: "right",
   })
     .to($.bars, {
       scaleX: 0,
     })
     .set($.bars, {
-      // transformOrigin: "left center",
-      transformOrigin: "left",
+      transformOrigin: "left center",
+      //   transformOrigin: "left",
     })
     .to($.bars, {
       scaleX: 1,
