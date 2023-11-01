363774bd57fdd469b311f002cf2df7ff34380e84
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-11-01 13:30:13 +0900

    test:#02 Get element related to btn

--- src/001_dev_style/scripts/component/menu.js
+++ src/001_dev_style/scripts/component/menu.js
@@ -1,9 +1,16 @@
+import { iNode } from "../helper/iNode.js";
+
 const menu = {
   init,
 };
 
 function init() {
-  console.log("menu init");
+  const btn = iNode.qs(".btn-menu");
+  const inner = iNode.qs(".btn-menu_inner");
+  const wrap = iNode.qsa(".btn-menu_wrap");
+  const bar = iNode.qsa(".btn-menu_bar");
+
+console.log(btn, inner, wrap, bar);
 }
 
 export default menu;
