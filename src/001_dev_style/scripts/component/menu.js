43fe982bd86b0318a67d50d6244da055900e26c5
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-11-01 14:10:48 +0900

    feat:#05 gsap defaults property setting

--- src/001_dev_style/scripts/component/menu.js
+++ src/001_dev_style/scripts/component/menu.js
@@ -11,8 +11,8 @@ function init() {
   $.container = iNode.qs("#global-container");
   $.btn = iNode.qs(".btn-menu");
   $.inner = iNode.qs(".btn-menu_inner");
-  $.wrap = iNode.qsa(".btn-menu_wrap");
-  $.bar = iNode.qsa(".btn-menu_bar");
+  $.wraps = iNode.qsa(".btn-menu_wrap");
+  $.bars = iNode.qsa(".btn-menu_bar");
   $.page = iNode.qs("#page-container");
 
   _handlePointerDownAndMouseEnter();
@@ -26,21 +26,26 @@ function _handlePointerDownAndMouseEnter() {
 function _handlePointerDown() {}
 
 function _handleMouseEnter() {
-  const tl = gsap.timeline();
-  tl.to($.wrap[0], {
-    default: { duration: 0.2 },
-    x: -100,
-    y: 100,
-    ease: "power1.inOut",
-    onComplete: () => {
-      gsap.to($.wrap[0], {
-        duration: 0.2,
-        x: 0,
-        y: 0,
-        ease: "power1.inOut",
-      });
-    },
+  const tl = gsap.timeline({defaults: { duration: 0.2, stagger: 0.1 }});
+  tl.set($.bars, {
+    transformOrigin: "center",
+  }).to($.bars, {
+    scaleX: 0,
   });
+  //   tl.to($.wrap[0], {
+  //     default: { duration: 0.2 },
+  //     x: -100,
+  //     y: 100,
+  //     ease: "power1.inOut",
+  //     onComplete: () => {
+  //       gsap.to($.wrap[0], {
+  //         duration: 0.2,
+  //         x: 0,
+  //         y: 0,
+  //         ease: "power1.inOut",
+  //       });
+  //     },
+  //   });
 }
 
 export default menu;
