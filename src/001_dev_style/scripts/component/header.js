a8525241f9b06722efe802a7d1fc0bc82c402ca0
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-11-09 19:15:52 +0900

    feat:#06 varにマイナスつけることができないので、−１を乗算して解決しました

--- src/001_dev_style/scripts/component/header.js
+++ src/001_dev_style/scripts/component/header.js
@@ -2,15 +2,10 @@ import { iNode } from "../helper";
 
 const header = {
   calcHeaderHeight,
-  //   test,
 };
 
 function calcHeaderHeight() {
   iNode.styleSetProperty("Header");
 }
-
-// function test() {
-//   iNode.styleSetProperty();
-// }
 
 export { header };
