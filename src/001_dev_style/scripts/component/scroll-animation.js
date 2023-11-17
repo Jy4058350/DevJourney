4ed9fcb9c629352e9a38d88b4e8896c68fbbf105
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-11-17 11:26:10 +0900

    test:#02 attempt to implement style attribute

--- src/001_dev_style/scripts/component/scroll-animation.js
+++ src/001_dev_style/scripts/component/scroll-animation.js
@@ -1,9 +1,13 @@
-import gsap from 'gsap';
+import gsap from "gsap";
 
-gsap.set(':root', {
-    '--color-text': '#dadada',
-    '--color-bg': '#1a1a1a',
-    '--color-sec': '#2b2b2b',
-    '--color-main': '#f2f2f2',
-    '--color-accent': '#ff0000',
-})
+gsap.set(":root", {
+  "--color-text": "#dadada",
+  "--color-bg": "#1a1a1a",
+  "--color-sec": "#2b2b2b",
+  "--color-main": "#f2f2f2",
+  "--color-accent": "#ff0000",
+});
+
+gsap.set("#header", {
+  "--header-height": "42px",
+});
