8f8f26e6aa3bf1c27c91e045ab87ef7ab1f08ddf
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-11-28 10:20:22 +0900

    test:#02 Use beforebegin and afterend , appearance is to be expected

--- src/001_dev_style/scripts/component/theme.js
+++ src/001_dev_style/scripts/component/theme.js
@@ -16,8 +16,12 @@ function init() {
         const action = data[key];
         console.log(action);
         // el.textContent = `${drawerValue}cart(`;
-        el.insertAdjacentHTML("beforeend", "cart(");
-        el.insertAdjacentHTML("afterend", ")");
+        // el.insertAdjacentHTML("beforeend", "cart(");
+        // el.insertAdjacentHTML("afterend", ")");
+        if (cartCountEl) {
+          cartCountEl.insertAdjacentHTML("beforebegin", "cart(");
+          cartCountEl.insertAdjacentHTML("afterend", ")");
+        }
       }
     }
   });
