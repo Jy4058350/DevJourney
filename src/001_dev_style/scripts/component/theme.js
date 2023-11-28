ea2ec12f8f863c9e46f7d0568acc46786c9a5339
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-11-28 10:14:47 +0900

    chore:#01 From now  attempt to write a description that inserts text before and after the span tag

--- src/001_dev_style/scripts/component/theme.js
+++ src/001_dev_style/scripts/component/theme.js
@@ -6,11 +6,21 @@ const theme = {
 
 function init() {
   const els = iNode.qsa("[data-action");
+  const cartCountEl = iNode.qs(".Header__CartCount");
   els.forEach((el) => {
     const data = el.dataset;
     console.log(data);
+    for (let key in data) {
+      if (!key.startsWith("drawer")) continue;
+      if (key.startsWith("drawer")) {
+        const action = data[key];
+        console.log(action);
+        // el.textContent = `${drawerValue}cart(`;
+        el.insertAdjacentHTML("beforeend", "cart(");
+        el.insertAdjacentHTML("afterend", ")");
+      }
+    }
   });
 }
-
 
 export { theme };
