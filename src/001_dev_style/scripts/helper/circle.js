1a9ae0ac66f5fe90b48f0671869a4c088170f484
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-12-08 09:16:20 +0900

    chore*#03 create multiple circle

--- src/001_dev_style/scripts/helper/circle.js
+++ src/001_dev_style/scripts/helper/circle.js
@@ -1,17 +1,30 @@
+import gsap from "gsap";
+import { iNode } from "./iNode.js";
+
 const circle = {
   createCircle,
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = gsap.timeline();
+  const circleContainer = iNode.getElById("circleContainer");
+
+  for (let i = 0; i < 16; i++) {
+    // slider.to(`.slide:nth-child(${i + 1})`, { x: `-${i * 100}%`, duration: 1 });
+    createCircle(i + 1);
+  }
+});
 
 function createCircle() {
   const circle = document.createElement("div");
   circle.classList.add("circle");
   circleContainer.appendChild(circle);
   console.log("add circle");
-  
-  circle.addEventListener("click", function() {
-      Slider.seek(index-1);
-      console.log("click");
-  })
+
+  circle.addEventListener("click", function () {
+    Slider.seek(index - 1);
+    console.log("click");
+  });
 }
 
 export { circle };
