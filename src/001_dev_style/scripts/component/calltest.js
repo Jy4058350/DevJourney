db6ad853864a3b7119b40886eadc130f9263a09c
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2023-12-30 07:49:03 +0900

    test:#02 call js

---
+++ src/001_dev_style/scripts/component/calltest.js
@@ -0,0 +1,11 @@
+// const calltest = {};
+
+const calltest = {
+  initCalltest,
+};
+
+function initCalltest() {
+  console.log("calltest.js");
+}
+
+export { calltest };
--- src/001_dev_style/scripts/component/home-news.js
+++ src/001_dev_style/scripts/component/home-news.js
@@ -1,8 +1,45 @@
 import { iNode } from "../helper";
 
+// console.log("home-news.js");
+
 const homeNews = {
-  init,
+//   init,
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOMContentLoaded");
+
+  // Get necessary elements
+  const slider = iNode.qs(".rotation-slider");
+  const prevButton = iNode.qs(".home-news-control-button.Previous");
+  const nextButton = iNode.qs(".home-news-control-button.Next");
+  console.log("slider", slider);
+  console.log("prevButton", prevButton);
+  console.log("nextButton", nextButton);
+
+  // Set the initial index
+  let currentIndex = 0;
+
+  // Function to update the slider position based on the current index
+  function updateSlider() {
+    const itemWidth = slider.firstElementChild.clientWidth;
+    const newPosition = -currentIndex * itemWidth;
+    slider.style.transform = `translateX(${newPosition}px)`;
+  }
+
+  // Event listener for the previous button
+  prevButton.addEventListener("click", function () {
+    currentIndex = Math.max(currentIndex - 1, 0);
+    updateSlider();
+  });
+
+  // Event listener for the next button
+  nextButton.addEventListener("click", function () {
+    const numItems = slider.children.length;
+    currentIndex = Math.min(currentIndex + 1, numItems - 1);
+    updateSlider();
+  });
+});
 
 function init() {
   // Get necessary elements
@@ -13,7 +50,8 @@ function init() {
   console.log("prevButton", prevButton);
   console.log("nextButton", nextButton);
 
-  document.addEventListenre("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOMContentLoaded");
     // Set the initial index
     let currentIndex = 0;
 
