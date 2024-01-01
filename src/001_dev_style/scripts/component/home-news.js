b9e8cac609f3c3a1acd2c48af53a940649faadfb
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-01-01 10:21:31 +0900

    fix:#02 dragging is not working

--- src/001_dev_style/scripts/component/home-news.js
+++ src/001_dev_style/scripts/component/home-news.js
@@ -9,22 +9,76 @@ let isDragging = false;
 let startX = 0;
 let currentX = 0;
 let angle = 0;
+// let sliders = null;
+let numItems = 0;
+let currentIndex = 0;
+
+const $ = {};
 
 function init() {
   // Get necessary elements
-  const sliders = iNode.qs(".rotation-slider");
-  const prevButton = iNode.qs(".home-news-control-button.Previous");
-  const nextButton = iNode.qs(".home-news-control-button.Next");
-  initDOM(sliders, prevButton, nextButton);
+  $.sliders = iNode.qs(".rotation-slider");
+  $.prevButton = iNode.qs(".home-news-control-button.Previous");
+  $.nextButton = iNode.qs(".home-news-control-button.Next");
+  initDOM($.sliders, $.prevButton, $.nextButton);
 
-  sliders.addEventListener("mousedown", (e) => {
-    isDragging = true; // This is the flag to check if the mouse is being draged
-    startX = e.clientX; // Get the initial position of the mouse
-    currentX = e.clientX; // It initializes the variable currentX with the same value as starX
-  });
+  $.sliders.addEventListener("mousedown", handleMouseDown);
 
-  return { sliders, prevButton, nextButton };
+  return $;
 }
+
+// from here
+
+function handleMouseDown(e, sliders) {
+  isDragging = true; // This is the flag to check if the mouse is being draged
+  startX = e.clientX; // Get the initial position of the mouse
+  currentX = e.clientX; // It initializes the variable currentX with the same value as starX
+
+  document.addEventListener("mousemove", handleMouseMove(e, sliders));
+
+  //   currentX = e.clientX;
+}
+
+function handleMouseMove(e, sliders) {
+  if (isDragging && sliders) {
+    const diffX = e.clientX - currentX;
+    currentX = e.clientX;
+    console.log(e.clientX);
+    console.log(currentX);
+
+    if (sliders.clientWidth !== 0) {
+      const newRotation =
+        -(currentIndex * angle) + (diffX / sliders.clientWidth) * 360;
+      sliders.style.transition = "transform 0s";
+      sliders.style.transform = `translateX(${newRotation}%)`;
+    }
+  }
+}
+
+document.addEventListener("mouseup", (e) => {
+  if (isDragging && $.sliders) {
+    isDragging = false;
+
+    $.sliders.style.transition = "transform 0.4s ease-in-out";
+    const currentTransform = parseFloat(
+      $.sliders.style.transform.replace("%", "")
+    );
+    currentIndex = Math.round(-$.sliders.style.transform / angle) % numItems;
+    updateSlider();
+  }
+});
+
+document.addEventListener("mouseleave", (e) => {
+  if (isDragging && $.sliders) {
+    isDragging = false;
+
+    $.sliders.style.transition = "transform 0.4s ease-in-out";
+    currentIndex = Math.round(-$.sliders.style.transform / angle) % numItems;
+    updateSlider();
+  }
+});
+
+// until here
 
 function initDOM(sliders, prevButton, nextButton) {
   //   console.log("sliders", sliders);
@@ -32,70 +86,33 @@ function initDOM(sliders, prevButton, nextButton) {
   //   console.log("nextButton", nextButton);
 }
 
-function initEventListenres(sliders, prevButton, nextButton) {
+function initEventListenres() {
   // Set the initial index
-  let currentIndex = 0;
-  const numItems = sliders.children.length;
-
-  document.addEventListener("mousemove", (e) => {
-    if (isDragging) {
-      const diffX = e.clientX - currentX;
-      currentX = e.clientX;
-
-      if (sliders.clientWidth !== 0) {
-        const newRotation =
-          -(currentIndex * angle) + (diffX / sliders.clientWidth) * 360;
-        sliders.style.transition = "transform 0s";
-        sliders.style.transform = `translateX(${newRotation}%)`;
-      }
-    }
-  });
-
-  document.addEventListener("mouseup", (e) => {
-    if (isDragging) {
-      isDragging = false;
-
-      sliders.style.transition = "transform 0.4s ease-in-out";
-      const currentTransform = parseFloat(
-        sliders.style.transform.replace("%", "")
-      );
-      currentIndex = Math.round(-sliders.style.transform / angle) % numItems;
-      updateSlider();
-    }
-  });
-
-  document.addEventListener("mouseleave", () => {
-    if (isDragging) {
-      isDragging = false;
-
-      sliders.style.transition = "transform 0.4s ease-in-out";
-      currentIndex = Math.round(-sliders.style.transform / angle) % numItems;
-      updateSlider();
-    }
-  });
+  currentIndex = 0;
+  numItems = $.sliders.children.length;
 
   // Function to update the slider position based on the current index
   function updateSlider() {
-    const itemWidth = sliders.firstElementChild.clientWidth;
+    const itemWidth = $.sliders.firstElementChild.clientWidth;
     angle = 360 / numItems;
     const newRotation = -currentIndex * angle;
-    sliders.style.transition = "transform 0.4s ease-in-out";
-    sliders.style.transform = `translateX(${newRotation}%)`;
-    console.log(currentIndex);
+    $.sliders.style.transition = "transform 0.4s ease-in-out";
+    $.sliders.style.transform = `translateX(${newRotation}%)`;
+    // console.log(currentIndex);
     if (currentIndex === 0) {
-      prevButton.style.display = "none";
+      $.prevButton.style.display = "none";
     } else {
-      prevButton.style.display = "block";
+      $.prevButton.style.display = "block";
     }
     if (currentIndex === 3) {
-      nextButton.style.display = "none";
+      $.nextButton.style.display = "none";
     } else {
-      nextButton.style.display = "block";
+      $.nextButton.style.display = "block";
     }
   }
 
   // Event listener for the previous button
-  prevButton.addEventListener("click", function () {
+  $.prevButton.addEventListener("click", function () {
     currentIndex = (currentIndex - 1 + numItems) % numItems;
     console.log("currentIndex", currentIndex);
 
@@ -103,21 +120,40 @@ function initEventListenres(sliders, prevButton, nextButton) {
   });
 
   //
-  sliders.addEventListener("transitionend", function () {
+  $.sliders.addEventListener("transitionend", function () {
     // const itemWidth = sliders.firstElementChild.clientWidth;
     // const angle = 360 / numItems;
     // const newRotation = -currentIndex * angle;
-    sliders.style.transition = "none";
+    $.sliders.style.transition = "none";
     // sliders.style.transform = `translateX(${newRotation}%)`;
   });
 
   // Event listener for the next button
-  nextButton.addEventListener("click", function () {
+  $.nextButton.addEventListener("click", function () {
     currentIndex = (currentIndex + 1) % numItems;
     updateSlider();
   });
 
   updateSlider();
+}
+
+function updateSlider() {
+  const itemWidth = $.sliders.firstElementChild.clientWidth;
+  angle = 360 / numItems;
+  const newRotation = -currentIndex * angle;
+  $.sliders.style.transition = "transform 0.4s ease-in-out";
+  $.sliders.style.transform = `translateX(${newRotation}%)`;
+  // console.log(currentIndex);
+  if (currentIndex === 0) {
+    $.prevButton.style.display = "none";
+  } else {
+    $.prevButton.style.display = "block";
+  }
+  if (currentIndex === 3) {
+    $.nextButton.style.display = "none";
+  } else {
+    $.nextButton.style.display = "block";
+  }
 }
 
 export { homeNews };
