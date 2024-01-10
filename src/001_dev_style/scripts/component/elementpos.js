f79e64274daffbe442ac3de3219be8fc7d216286
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-01-10 11:05:15 +0900

    feat:#09 FooterTop and El above footeEl calculating is good

--- src/001_dev_style/scripts/component/elementpos.js
+++ src/001_dev_style/scripts/component/elementpos.js
@@ -66,11 +66,11 @@ function _calcGapFooterPos() {
     const nextHomeNewsRect = $.homeNews.getBoundingClientRect();
     const nextFooterRect = $.footer.getBoundingClientRect();
     // console.log("nextFvMainRect", nextFvMainRect);
-    // console.log("nextHomeNewsRect", nextHomeNewsRect);
-    // console.log("nextFooterRect", nextFooterRect);
+    console.log("nextHomeNewsRect", nextHomeNewsRect);
+    console.log("nextFooterRect", nextFooterRect);
 
-    const gap = nextFvMainRect.bottom - nextFooterRect.top;
-    // const gap = nextHomeNewsRect.bottom - nextFooterRect.top;
+    // const gap = nextFvMainRect.bottom - nextFooterRect.top;
+    const gap = nextHomeNewsRect.bottom - nextFooterRect.top;
 
 
     console.log("gap", gap);
