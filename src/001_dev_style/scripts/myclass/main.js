3d626a40aac98503a379026dd7b56ee52461fafc
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-01-11 17:35:33 +0900

    first

---
+++ src/001_dev_style/scripts/myclass/main.js
@@ -0,0 +1,11 @@
+import { iNode } from "../helper";
+
+class DOMManuipulatorClass {
+  constructor(header, fv, footre) {
+    this.header = header;
+    this.fv = fv;
+    this.footre = footre;
+  }
+}
+
+export default DOMManuipulatorClass;
