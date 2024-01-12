1d8a65c76a4a7cb87f4461fe3d2b2aeec8dd8d47
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-01-12 14:49:01 +0900

    feat:#16 The result of method was stored this.gap, and set use this.gap

--- src/001_dev_style/scripts/myclasses/footer.js
+++ src/001_dev_style/scripts/myclasses/footer.js
@@ -5,6 +5,7 @@ class FooterHandler {
     this.footer = footer;
 
     this.init();
+    this.gap = this.getGap();
   }
   init() {
     const siblings = iNode.getElById("footer").previousElementSibling;
@@ -12,27 +13,16 @@ class FooterHandler {
     console.log("siblingsBottom", this.siblingsBottom);
     this.footerTop = this.footer.getBoundingClientRect().top;
     console.log("footerTop", this.footerTop);
+    this.setFooterPos();
   }
 
   getGap() {
+    const gap = this.siblingsBottom - this.footerTop;
+    console.log("gap", gap);
+  }
 
-   
-    return new Promise((resolve) => {
-      // iNode.setCssProp("--footer-top", 0, $.footer);
-      const nextFvMainRect = $.fv.getBoundingClientRect();
-      const nextHomeNewsRect = $.homeNews.getBoundingClientRect();
-      const nextFooterRect = $.footer.getBoundingClientRect();
-      // console.log("nextFvMainRect", nextFvMainRect);
-      console.log("nextHomeNewsRect", nextHomeNewsRect);
-      console.log("nextFooterRect", nextFooterRect);
-
-      // const gap = nextFvMainRect.bottom - nextFooterRect.top;
-      const gap = nextHomeNewsRect.bottom - nextFooterRect.top;
-
-      console.log("gap", gap);
-      iNode.setCssProp("--footer-top", `${gap}`, $.footer);
-      resolve();
-    });
+  setFooterPos() {
+    iNode.setCssProp("--footer-top", `${this.gap}`, this.footer);
   }
 }
 
