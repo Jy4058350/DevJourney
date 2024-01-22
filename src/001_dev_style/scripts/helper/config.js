0d5de0d8f89178214dfd3fc15d5539ad4aabdf1b
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-01-22 12:40:48 +0900

    chore:#01 store mainEl in configObject at config.js

---
+++ src/001_dev_style/scripts/helper/config.js
@@ -0,0 +1,11 @@
+const config = {
+  // Setting of key ID attributes in HTML
+  //(selectors in styles/parts/_common.scss must also be changed)
+  $: {
+    canvas: "#canvas",
+    globalContainer: "#globalContainer",
+    pageContainer: "#pageContainer",
+  },
+};
+
+export { config };
