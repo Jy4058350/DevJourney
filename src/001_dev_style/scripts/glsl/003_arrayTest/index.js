7f409e1fa0347d2740c92a9c1a85529d05f0613d
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-01-24 01:52:58 +0900

    chore:#05 for working convertMapToArray data-webgl is one in bottom2.html

--- src/001_dev_style/scripts/glsl/003_arrayTest/index.js
+++ src/001_dev_style/scripts/glsl/003_arrayTest/index.js
@@ -4,73 +4,13 @@ import vertexShader from "./vertex.glsl";
 import fragmentShader from "./fragment.glsl";
 
 import { CustomObject } from "../CustomObject";
+import { iNode } from "../../helper";
 import world from "../world";
 
 class ExtendObject extends CustomObject {
   constructor({ texes, el, type, canvasRect, program }) {
     super({ texes, el, type, canvasRect });
-    // let textureMap = new Map();
-    // textureMap.set("tex1", texes.get("tex1"));
-    // textureMap.set("tex2", texes.get("tex2"));
-
-    // let texturesArray = Array.from(textureMap.values());
-    // let gl = world.renderer.getContext();
-
-    // let vertexShaderCode = this.fixVertex();
-    // let fragmentShaderCode = this.fixFragment();
-    // let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    // let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    // gl.shaderSource(vertexShader, vertexShaderCode);
-    // gl.shaderSource(fragmentShader, fragmentShaderCode);
-    // gl.compileShader(vertexShader);
-    // gl.compileShader(fragmentShader);
-    // if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    //   console.error(
-    //     "Error compiling vertex shader:",
-    //     gl.getShaderInfoLog(vertexShader)
-    //   );
-    // }
-    // if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    //   console.error(
-    //     "Error compiling fragment shader:",
-    //     gl.getShaderInfoLog(fragmentShader)
-    //   );
-    // }
-
-    // for (let i = 0; i < texturesArray.length; i++) {
-    //   let texture = gl.createTexture();
-    //   gl.bindTexture(gl.TEXTURE_2D, texture);
-    //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    //   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-    //   let image = new Image();
-    //   image.onload = function () {
-    //     gl.bindTexture(gl.TEXTURE_2D, texture);
-    //     gl.texImage2D(
-    //       gl.TEXTURE_2D,
-    //       0,
-    //       gl.RGBA,
-    //       gl.RGBA,
-    //       gl.UNSIGNED_BYTE,
-    //       image
-    //     );
-    //   };
-    //   image.src = texturesArray[i];
-
-    //   gl.activeTexture(gl.TEXTURE0);
-    //   gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    //   let u_imageLocation = gl.getUniformLocation(program, "u_image");
-    //   gl.uniform1i(u_imageLocation, 0 + i);
-    // }
-
-    // let textureUnits = [0, 1];
-    //   for (let i = 0; i < texturesArray.length; i++) {
-    //     let webglTexture = texturesArray[i].texture;
-    //     gl.activeTexture(gl.TEXTURE0 + textureUnits[i]);
-    //     gl.bindTexture(gl.TEXTURE_2D, webglTexture[i]);
-    //   }
+    this.convertMapToArray(texes);
   }
 
   convertMapToArray(texes) {
