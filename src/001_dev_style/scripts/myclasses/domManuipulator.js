d6710ba2b85d8267d07b99186874ee53a6ee425a
Jun Yamabe <124499230+Jy4058350@users.noreply.github.com>
2024-01-28 07:25:15 +0900

    feat:#07 isWideToggler と isNarrowToggler メソッドは、ほとんど同じコードを含んでいます。これらのメソッドを一つにまとめ、引数を使って振る舞いを変えると、コードの重複を減らすことができます。toggleScreen メソッドを新たに作成し、isWideToggler と isNarrowToggler メソッドを置き換えました。また、setElHeight メソッドの pinSpacer の存在チェックを1回に減らしました。

--- src/001_dev_style/scripts/myclasses/domManuipulator.js
+++ src/001_dev_style/scripts/myclasses/domManuipulator.js
@@ -20,7 +20,8 @@ class DOMManuipulatorClass {
   init() {
     // add resize event listener
     this.isWideScreen = this._getWindowWidth() > this._toEm(1280, 16);
-    this.firstView();
+    // this.firstView();
+    this.toggleScreen(this.isWideScreen);
     this.updateStyle();
   }
 
@@ -47,7 +48,7 @@ class DOMManuipulatorClass {
   }
 
   updateWideScreenStyle(headerHeight) {
-    this.isWideToggler(this.isWideScreen);
+    // this.isWideToggler(this.isWideScreen);
     if (this.fv) {
       this.fvHandler.raiseFv(headerHeight);
     }
@@ -56,7 +57,7 @@ class DOMManuipulatorClass {
   }
 
   updateNarrowScreenStyle(headerHeight) {
-    this.isNarrowToggler(this.isWideScreen);
+    // this.isNarrowToggler(this.isWideScreen);
     if (this.fv) {
       this.fvHandler.raiseFv(headerHeight);
     }
@@ -75,36 +76,64 @@ class DOMManuipulatorClass {
 
   // related headerClass follows
 
-  isWideToggler(isWideScreen) {
+  toggleScreen(isWideScreen) {
+    const displayValue = isWideScreen ? "none" : "block";
+    const opacityValue = isWideScreen ? 1 : 0;
+    const openValue = isWideScreen;
+
     iNode.toggleClass(
       dom.headerFlexItem,
       "Header__FlexItem--increaseSpace",
       isWideScreen
     );
-    iNode.toggleClass(dom.headerHorizontalList, "Header__MainNav--open", true);
-    iNode.toggleClass(dom.headerMainNav, "Header__MainNav--open", true);
-    iNode.toggleClass(dom.headerSecondNav, "Header__secondaryNav--open", true);
-    iNode.setStyles(dom.headerHorizontalList, { opacity: 1 });
-    iNode.setStyles(dom.headerEntrance, { display: "none" });
-    iNode.setStyles(dom.headerMainNav, { opacity: 1 });
-    iNode.setStyles(dom.headerSecondNav, { opacity: 1 });
-    iNode.setStyles(dom.headerIcon, { display: "none" });
+    iNode.toggleClass(
+      dom.headerHorizontalList,
+      "Header__MainNav--open",
+      openValue
+    );
+    iNode.toggleClass(dom.headerMainNav, "Header__MainNav--open", openValue);
+    iNode.toggleClass(
+      dom.headerSecondNav,
+      "Header__secondaryNav--open",
+      openValue
+    );
+    iNode.setStyles(dom.headerHorizontalList, { opacity: opacityValue });
+    iNode.setStyles(dom.headerEntrance, { display: displayValue });
+    iNode.setStyles(dom.headerMainNav, { opacity: opacityValue });
+    iNode.setStyles(dom.headerSecondNav, { opacity: opacityValue });
+    iNode.setStyles(dom.headerIcon, { display: displayValue });
   }
 
-  isNarrowToggler(isWideScreen) {
-    iNode.toggleClass(
-      dom.headerFlexItem,
-      "Header__FlexItem--increaseSpace",
-      isWideScreen
-    );
-    iNode.toggleClass(dom.headerHorizontalList, "Header__MainNav--open", false);
-    iNode.toggleClass(dom.headerMainNav, "Header__MainNav--open", false);
-    iNode.setStyles(dom.headerHorizontalList, { opacity: 0 });
-    iNode.setStyles(dom.headerEntrance, { display: "block" });
-    iNode.setStyles(dom.headerMainNav, { opacity: 0 });
-    iNode.setStyles(dom.headerSecondNav, { opacity: 0 });
-    iNode.setStyles(dom.headerIcon, { display: "block" });
-  }
+  // isWideToggler(isWideScreen) {
+  //   iNode.toggleClass(
+  //     dom.headerFlexItem,
+  //     "Header__FlexItem--increaseSpace",
+  //     isWideScreen
+  //   );
+  //   iNode.toggleClass(dom.headerHorizontalList, "Header__MainNav--open", true);
+  //   iNode.toggleClass(dom.headerMainNav, "Header__MainNav--open", true);
+  //   iNode.toggleClass(dom.headerSecondNav, "Header__secondaryNav--open", true);
+  //   iNode.setStyles(dom.headerHorizontalList, { opacity: 1 });
+  //   iNode.setStyles(dom.headerEntrance, { display: "none" });
+  //   iNode.setStyles(dom.headerMainNav, { opacity: 1 });
+  //   iNode.setStyles(dom.headerSecondNav, { opacity: 1 });
+  //   iNode.setStyles(dom.headerIcon, { display: displayValue });
+  // }
+
+  // isNarrowToggler(isWideScreen) {
+  //   iNode.toggleClass(
+  //     dom.headerFlexItem,
+  //     "Header__FlexItem--increaseSpace",
+  //     isWideScreen
+  //   );
+  //   iNode.toggleClass(dom.headerHorizontalList, "Header__MainNav--open", false);
+  //   iNode.toggleClass(dom.headerMainNav, "Header__MainNav--open", false);
+  //   iNode.setStyles(dom.headerHorizontalList, { opacity: 0 });
+  //   iNode.setStyles(dom.headerEntrance, { display: "block" });
+  //   iNode.setStyles(dom.headerMainNav, { opacity: 0 });
+  //   iNode.setStyles(dom.headerSecondNav, { opacity: 0 });
+  //   iNode.setStyles(dom.headerIcon, { display: "block" });
+  // }
 
   getHeaderHeight() {
     const header = iNode.qs("#header");
@@ -120,7 +149,6 @@ class DOMManuipulatorClass {
     this.header.style.maxHeight = `${value}px`;
 
     const pinSpacer = iNode.qs(".pin-spacer");
-
     if (pinSpacer) {
       pinSpacer.style.height = `${value}px`;
       pinSpacer.style.maxHeight = `${value}px`;
